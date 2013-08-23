822f2005f2098f301c8e88b200fdb57c1c1a499e
dvbrown <dvbrown@student.unimelb.edu.au>
2013-08-23 17:56:57 +1000

    more parsing

--- 130724_filterMafFile.R
+++ 130724_filterMafFile.R
@@ -1,6 +1,28 @@
-#A script to filter the maf file into a vcf file? Some file that is good for the tools.
+#!/usr/bin/env Rscript
+library(optparse)
+#setwd('~/Documents/eQTL/')
 
-mutations = read.delim('~/Documents/public-datasets/firehose/mafDashboard/2013_06_27/test.maf', skip=4)
+# specify our desired options in a list
+# by default OptionParser will add an help option equivalent to
+# make_option(c("-h", "--help"), action="store_true", default=FALSE,
+# help="A script to filter the maf file into a vcf file? Some file that is good for the tools.")
+option_list <- list(
+  make_option(c("-v", "--verbose"), action="store_true", default=FALSE,
+              help="An Rscript to that matches the column names in a mutation SNP like file with a gene expression matrix"),
+  make_option(c("-m", "--mafFile"), action="store", type='character', default='./130823_fullData/step4_gbm_liftover.aggregated.capture.tcga.uuid.maf2.4.migrated.somatic.txt',
+              help="The mutation TCGA file"),
+  make_option(c("-o", "--output"), action="store", type='character', default='./130823_fullData/output.txt',
+              help="The filtered maf file with fileds of interest")
+)
+# get command line options, if help option encountered print help and exit,
+# otherwise if options not found on command line then set defaults,
+opt <- parse_args(OptionParser(option_list=option_list))
+
+#########################################################################################
+#A script to filter the maf file into a vcf file? Some file that is good for the tools.
+#########################################################################################
+
+mutations = read.delim(opt$mafFile, skip=4)
 
 mutFilter = mutations[,c(1,9,16,26)]
 #rm(mutations)
@@ -14,4 +36,4 @@ positionFinal = position[,c(1,2,5)]
 mutFilter$Variant_Classification = as.character(mutFilter$Variant_Classification)
 #remove those mutations classed as silent
 mutClass = mutFilter[(mutFilter$Variant_Classification != 'Silent'),]
-write.table(mutClass,'./130806_filteredMutationsTCGA.txt',sep='\t',row.names=FALSE)
+write.table(mutClass,opt$output,sep='\t',row.names=FALSE)
