c9854691ef5a031baa9fffc8b85b74e24e42bf4a
dvbrown <dvbrown@student.unimelb.edu.au>
2013-08-26 17:47:16 +1000

    filtering normalised data

--- 130826_filterContentseQTL.R
+++ 130826_filterContentseQTL.R
@@ -18,9 +18,13 @@ option_list <- list(
               help="A tab delimited text file of the gene expression matrix"),
   make_option(c("-s", "--snpFiltered"), action="store", type='character', default='outputM.txt',
               help="The Filtered mutation file"),
-  make_option(c("-c", "--cuttoff"), action="store",type = 'integer', default=10,
-              help="The minimum percntage prevalence of the mutation in the cohort"),
+  make_option(c("-c", "--cuttoffMut"), action="store",type = 'numeric', default=0.05,
+              help="The minimum percentage prevalence of the mutation in the cohort. Default = 5%"),
+  make_option(c("-n", "--cuttoffGene"), action="store",type = 'numeric', default=0.2,
+              help="The maximum percentage of patients with NA expression of a gene in the cohort. Default = 20% "),
   make_option(c("-o", "--outGene"), action="store", type='character', default='outputG.txt',
+              help="The filtered gene expression matrix"),
+  make_option(c("-p", "--outMut"), action="store", type='character', default='outputM.txt',
               help="The filtered gene expression matrix")
 )
 # get command line options, if help option encountered print help and exit,
@@ -36,10 +40,19 @@ mutations = read.delim(opt$mutationFile, row.names=1)
 genes = read.delim(opt$geneExpressionMatrix, row.names=1)
 genes$NAs = apply(genes, 1, countNAs)
 
-#filter for the prevalence of a mutation in a patient
-patientNumber = length(mutations)
-mutFilter = subset(mutations, rowSums(mutations) >= patientNumber/opt$cuttoff)
+#filter for the prevalence of a mutation in a patient. Remove genes with less n % mutation rate in GBM
+patientNumber = as.numeric(length(mutations))
+mutations$mutNum = rowSums(mutations)
+mutFilter = subset(mutations, rowMeans(mutations) >= opt$cuttoffMut)
 
-#Filter the RNAseq data for expression. Used the RSEM log2 normalised data
-geneFilter = subset(genes, genes$NAs <= patientNumber/4)
+barplot(mutFilter$mutNum, names.arg=row.names(mutFilter), ylab='Number of patients with mutation',
+        main='Frequency of mutations')
+
+mutFilter = mutFilter[,c(1:patientNumber)]
+
+#Filter the RNAseq data for expression. Remove genes with more than n NA values (ie not expressed). Used the RSEM log2 normalised data
+geneFilter = subset(genes, genes$NAs <= patientNumber * opt$cuttoffGene)
 geneFilter = geneFilter[,c(1:patientNumber)]
+
+write.table(geneFilter, file=opt$outGene, sep='\t', row.names=T)
+write.table(mutFilter, file=opt$outMut, sep='\t', row.names=T)
