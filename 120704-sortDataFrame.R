ca377da1f0396f0d4be8754c2352d8161e622fcc
dvbrown <dvbrown@student.unimelb.edu.au>
2013-08-30 14:41:37 +1000

    moving lines around

--- 120704-sortDataFrame.R
+++ 120704-sortDataFrame.R
@@ -1,5 +1,5 @@
 sort.dataframe <- function(dataframe, columnNumber, highFirst=TRUE) {
   #if highFirst is true the return the list from high to low
-  new.dataframe = dataframe[with(dataframe, order(dataframe[columnNumber], decreasing=highFirst)),]
+  new.dataframe = dataframe[with(dataframe, order(dataframe[,columnNumber], decreasing=highFirst)),]
   return (new.dataframe)
 }
