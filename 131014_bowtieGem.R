161cc41c6abf1055ce68fb5c818edf39c3fee909
dvbrown <dvbrown@student.unimelb.edu.au>
2013-10-15 11:32:42 +1100

    got to MDS plot

--- 131014_bowtieGem.R
+++ 131014_bowtieGem.R
@@ -1,3 +1,4 @@
+library(edgeR)
 setwd('~/Documents/RNAdata/danBatch1/bowtieGem/')
 files = list.files(pattern='*.txt')
 
@@ -6,14 +7,21 @@ df1 = cbind(f[[1]],f[[2]],f[[3]],f[[4]],f[[5]],f[[6]])
 df = df1[,c(2,4,6,8,10,12)]
 row.names(df) = df1[,1]
 colnames(df) = c('GIC_011', 'GIC_020', 'GIC_034', 'GIC_035', 'GIC_039', 'GIC_041')
+labels = c('011', '020', '034', '035', '039', '041')
 
 noFeatures = tail(df)
+df = df[c(1:203288),]
 
-dataSub = data[c(1:80921),]
-row.names(dataSub) = data[,1]
-dataSub$X15 = as.numeric(dataSub$X15)
+#Build EdgeR objects
+condition = c('long', 'long', 'long', 'short', 'short', 'short')
+counts = DGEList(counts=df, group=condition)
 
-s = sum(dataSub$X15)
-tail(data)
+#critical step In edgeR, it is recommended to remove features without at least 1 read per million in n of the samples,
+#where n is the size of the smallest group of replicates (here, n = 3 for the knockdown group)
+cpms = cpm(counts)
+keep = rowSums(cpms >1) >=3
+counts = counts[keep,]
 
-data1 = dataSub[dataSub$X15 > 0,]
+#nomalise, plot MDS
+d = calcNormFactors(counts)
+plotMDS(d, labels=labels, col = c("darkgreen","blue")[factor(condition)], cex=0.8, main='MDS plot GIC RNA-seq batch1')
