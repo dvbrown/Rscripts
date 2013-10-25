b8a8ccfd3f043687600ce827023264d48fc7414f
dvbrown <dvbrown@student.unimelb.edu.au>
2013-10-25 16:43:00 +1100

    cut extra columns from data file seems to make cytoscape match better

--- 131025_buildCytoscapeNetwork.R
+++ 131025_buildCytoscapeNetwork.R
@@ -7,3 +7,10 @@ geneData$description = gsub('Source:HGNC Symbol;Acc', '', geneData$description)
 nw = networkData[,c(1,2,15)]
 
 output = merge(nw, geneData, by.x='X.node1', by.y='external_gene_id')
+output = output[,c(1,2,3,4,5,6,9)]
+colnames(output) = c('source', 'target', 'evScore', 'description', 'LFC', 'CPM', 'FDR')
+
+write.table(output, './networks/131025_cytoscapeInput.txt', sep='\t', row.names=F, quote=F)
+
+gd = geneData[,c(1,3,4,7)]
+write.table(gd, './networks/131025_cytoscapeData.txt', sep='\t', row.names=F, quote=F)
