431e9a8c594b3e38bfd4c5800b200167c16c0798
dvbrown <dvbrown@student.unimelb.edu.au>
2013-10-29 13:10:53 +1100

    going through EdeR user guide, adding useful bits

--- 131014_bowtieGemGLM.R
+++ 131014_bowtieGemGLM.R
@@ -30,7 +30,10 @@ counts = counts[keep,]
 
 #nomalise, plot MDS
 d = calcNormFactors(counts)
-plotMDS(d, labels=labels, col = c("darkgreen","blue")[factor(condition)], cex=1.25, main='MDS plot GIC RNA-seq batch1')
+
+# Plots according to the biological coefficient of variation
+plotMDS(d, labels=labels, col = c("darkgreen","blue")[factor(condition)], cex=1.25, 
+        main='MDS plot GIC RNA-seq batch1')
 legend('topright', legend=c('Long-term','Short-term'), fill=c("darkgreen","blue"), cex=0.5)
 
 boxplot(cpm(counts, log=T), main='Normalised counts RNA-seq batch1', ylab='Log2 CPM', col=colors, cex=1.25, las=2)
@@ -39,9 +42,11 @@ boxplot(cpm(counts, log=T), main='Normalised counts RNA-seq batch1', ylab='Log2 
 design = model.matrix(~ libPrep + age + group, dm)
 
 # Estimate dispersion, adjusting for the GLM
-d2 = estimateGLMTrendedDisp(d, design)
+d2 = estimateGLMCommonDisp(d, design, verbose=T)
+# Condense the disersion estimate by estiamting a trend for count size
+d2 = estimateGLMTrendedDisp(d2, design)
+# Empriical Bayes tagwise dispersion
 d2 = estimateGLMTagwiseDisp(d2, design)
-d2 = estimateGLMCommonDisp(d2, design)
 
 # Plot the dispersions. Tagwise vars is blue scatter. NB line is blue. Poisson line is black. Raw variance is maroon
 plotMeanVar(d2, show.tagwise.vars=TRUE, NBline=TRUE, main='Fitted dispersion GIC RNA-seq batch 1')
@@ -51,19 +56,32 @@ legend('topleft', legend=c('Poisson line', 'Neg Binomial line', 'Tagwise disp', 
 plotBCV(d2, main='Biological variation GIC RNA-seq batch 1')
 
 # Fit the GLM, interested in the group difference
+# Fit the GLM per gene
 f = glmFit(d2, design)
 de = glmLRT(f, coef=4)
 
 tt = topTags(de, n=nrow(d))
 head(tt$table)
+summary(decideTestsDGE(de))
+
+# The quasi likihood metod deosn't give any differentially expressed genes
+# fit <- glmQLFTest(d2,design,robust=TRUE,plot=TRUE)
+# t = topTags(fit, n=nrow(d))
+# head(t$table)
+# summary(decideTestsDGE(fit))
 
 # Create a graphical summary, such as an M (log-fold change) versus A (log-average expression) plot
 rn = rownames(tt$table)
 deg = rn[tt$table$FDR < .05]
 plotSmear(d, de.tags=deg, main='MA plot GIC RNA-seq batch1')
+abline(h=c(-1, 1), col="blue")
 
 nc = cpm(d, normalized.lib.sizes=TRUE)
 head(nc[rn,order(labels)],5)
+
+# Have a look at the counts of the most significant DE genes. Look for consistency in the counts
+o = order(de$table$PValue)
+cpm(d2)[o[1:50],]
 
 result = ensembl_2_geneName(tt$table)
 result = sort.dataframe(result, 8, highFirst=FALSE)
