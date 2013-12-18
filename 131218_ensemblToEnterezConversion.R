65aef068b2986399218ce7ae22afdc4736495646
dvbrown <dvbrown@student.unimelb.edu.au>
2013-12-18 11:48:57 +1100

    got the geneID conversion working

--- 131218_ensemblToEnterezConversion.R
+++ 131218_ensemblToEnterezConversion.R
@@ -4,10 +4,10 @@
 library(annotate)
 library(org.Hs.eg.db)
 
-ensembl2enterezConvert = function(deGenes) {
-  ensembl2enterez = select(org.Hs.eg.db, keys=deGenes$ensembl_gene_id, cols='ENTEREZID', keytype='ENSEMBL')
+ensembl2enterezConvert = function(genesTestedforDE) {
+  ensembl2enterez = select(org.Hs.eg.db, keys=row.names(genesTestedforDE), cols='ENTREZID', keytype='ENSEMBL')
   ensembl2enterez = ensembl2enterez[!is.na(ensembl2enterez),]
-  ensembl2enterez = ensembl2enterez[!is.na(ensembl2enterez$ENTEREZID),]
-  ensembl2enterez = ensembl2enterez[!duplicated(ensembl2enterez$ENTEREZID),]
+  ensembl2enterez = ensembl2enterez[!is.na(ensembl2enterez$ENTREZID),]
+  ensembl2enterez = ensembl2enterez[!duplicated(ensembl2enterez$ENTREZID),]
   return (ensembl2enterez)
 }
--- 131218_spiaPathwayEnrichment.R
+++ 131218_spiaPathwayEnrichment.R
@@ -1,6 +1,13 @@
 # Measuring pathway enrichment using SPIA in RNA-seq batch1
 library(SPIA)
+source('~/Documents/Rscripts/131218_ensemblToEnterezConversion.R')
 setwd('~/Documents/RNAdata/danBatch1/bowtieGem/revHTSeq/GLMedgeR/')
 
-# Use the liberal differentially expressed genes
-deGenes = read.delim('131021_shortVSlongLiberalDE.txt')
+# Import the full dataset
+data = read.delim('131021_shortVSlong.txt', row.names=2)
+data = data[,c(2:8)]
+data$ensembl = row.names(data)
+
+# Add the enterez IDs
+ensemblEnterezMap = ensembl2enterezConvert(data)
+rnaseq.data.ent <- cbind(entid = ensemblEnterezMap$ENTREZID, data[ensemblEnterezMap$ENSEMBL,])
