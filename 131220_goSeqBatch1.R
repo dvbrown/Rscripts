90029000e5694479c88d3b361050cb03726ba8c2
dvbrown <dvbrown@student.unimelb.edu.au>
2013-12-20 16:37:35 +1100

    added GO term enrichment testing

--- 131220_goSeqBatch1.R
+++ 131220_goSeqBatch1.R
@@ -16,9 +16,9 @@ null_model_down <- nullp(down.genes, genome = "hg19", id = "ensGene")
 
 # Calculate enrichment of KEGG enrichment
 kegg_enrichmentUp <- goseq(null_model_up, genome = "hg19", id = "ensGene", 
-                           test.cats = c("KEGG","GO:CC", "GO:BP","GO:MF"), method = "Wallenius")
+                           test.cats = c("KEGG", "GO:BP","GO:MF"), method = "Wallenius")
 kegg_enrichmentDown <- goseq(null_model_down, genome = "hg19", id = "ensGene", 
-                             test.cats = c("KEGG","GO:CC", "GO:BP","GO:MF"), method = "Wallenius")
+                             test.cats = c("KEGG", "GO:BP","GO:MF"), method = "Wallenius")
 
 # Control for FDR
 kegg_fdrs_up <- p.adjust(kegg_enrichmentUp[, 2], method = "BH")
@@ -43,3 +43,10 @@ kegg_enrichment <- cbind(kegg_enrichment_down, pathway = keggid2name[kegg_enrich
 enriched_pathwaysDown <- kegg_enrichment[kegg_enrichment_down$FDR < 0.1, ]
 dim(enriched_pathwaysDown)[1]
 write.table(enriched_pathwaysDown, './131220_enrichedKEGGpathwaysLongTerm.txt', sep='\t')
+
+# Now annotate IDs
+library(biomaRt)
+mart <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+results <- getBM(attributes = c("refseq_dna", "go_molecular_function_id",
+                                "go_molecular_function__dm_name_1006"), filters = "refseq_dna",
+                 values = enriched_pathwaysDown$category, mart = mart)
