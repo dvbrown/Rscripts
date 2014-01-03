540f4b19d1506c98ae0d385677d2bd8a21ec55eb
dvbrown <dvbrown@student.unimelb.edu.au>
2014-01-03 14:55:37 +1100

    finished spia, got desired result

--- 131219_spiaPathwayEnrichmentForPaul.R
+++ 131219_spiaPathwayEnrichmentForPaul.R
@@ -86,26 +86,24 @@ intData = sumData[sumData$CREBcounts > 0,]
 
 ########################################## Now the SPIA enrichment ########################################
 
-result.spia = spia(de=de.genes, all=all.genes, organism='hsa', nB=2000, plots=F)
-result.spia$Name <- substr(spia.results$Name, 1, 25)
+# Set up data for spia
+genes <- abs(intData$rowMean) > 2
+names(genes) <- intData$ENTREZID
+all.genes <- crebTargets$gene
+
+result.spia = spia(de=genes, all=all.genes, organism='hsa', nB=2000, plots=F)
 result.spia[1:20, -12]
 
 # tA is the equivaent of fold hange pertubation in the pathway.
 # pXXX is all the various FDR corrections
 
-spiaNoNAs = result.spia[!is.na(result.spia$pPERT),]
-plotP(spiaNoNAs, threshold=0.1)# x.lab='Enrichment score', ylab='Pertubation score', main='Disrupted pathways in short-term surviving GICs')
+plotP(result.spia, threshold=0.1)# x.lab='Enrichment score', ylab='Pertubation score', main='Disrupted pathways in short-term surviving GICs')
 
 # output the results of the analysis
-write.table(result.spia, './output/spia/131218_spiaResults.txt', sep='\t', row.names=F)
-write.table(spiaNoNAs, './output/spia/131218_spiaNoNAsResults.txt', sep='\t', row.names=F)
+write.table(result.spia, './output/spia/140103_spiaResults.txt', sep='\t', row.names=F)
+#write.table(spiaNoNAs, './output/spia/140103_spiaNoNAsResults.txt', sep='\t', row.names=F)
 
 # Visualise the KEGG pathway data in R
 # Use the results of plotP to get the pathway ids
 setwd('./output/spia/')
-pv.out = pathview(gene.data=de.genes, pathway.id='04970', species='hsa', out.suffix='salivarySecretion', kegg.native=T)
-pv.out = pathview(gene.data=de.genes, pathway.id='04512', species='hsa', out.suffix='ECMreceptor', kegg.native=T)
-pv.out = pathview(gene.data=de.genes, pathway.id='05222', species='hsa', out.suffix='smallCellLungCancer', kegg.native=T)
-pv.out = pathview(gene.data=de.genes, pathway.id='04020', species='hsa', out.suffix='calciumSignalling', kegg.native=T)
-pv.out = pathview(gene.data=de.genes, pathway.id='04080', species='hsa', out.suffix='neuroactiveLigand', kegg.native=T)
-
+pv.out = pathview(gene.data=intData$rowMean, pathway.id='04110', species='hsa', out.suffix='Cell cycle', kegg.native=T)
