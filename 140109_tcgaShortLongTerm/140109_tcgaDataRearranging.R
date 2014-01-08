a67df593e663dca0c81b1794e70197c1bc425dd5
dvbrown <dvbrown@student.unimelb.edu.au>
2014-01-09 10:55:49 +1100

    munged data into 3 data frames

--- 140109_tcgaShortLongTerm/140109_tcgaDataRearranging.R
+++ 140109_tcgaShortLongTerm/140109_tcgaDataRearranging.R
@@ -1,5 +1,5 @@
 # This script will import some TCGA data and test for differential expression between long and short term survivors
-
+source('~/Documents/Rscripts/120704-sortDataFrame.R')
 setwd('~/Documents/public-datasets/firehose/stddata__2013_12_10/GBM/20131210_dataReformatting/')
 list.files()
 
@@ -7,6 +7,14 @@ clinical = read.delim('140108_clinicalDataPart1.txt')
 clinical2 = read.delim('140108_clinicalDataPart2.txt', skip=0)
 clinical2 = t(clinical2)
 
-affy = read.delim('140108_affymetrixGem.txt')
+affy = read.delim('140108_affymetrixGem.txt', row.names=1)
 agilent= read.delim('140108_agilentPart1gem.txt')
 agilent2 = read.delim('140108_agilentPart2gem.txt')
+# Agilent2 is out of order
+agilent2 = sort.dataframe(agilent2, 1, highFirst=FALSE)
+
+# Bind the 2 parts of agilent together
+agilentTotal = merge.data.frame(agilent, agilent2, by.x='Hybridizatio', by.y='Hybridizatio')
+row.names(agilentTotal) = agilentTotal[,1]
+agilentTotal = agilentTotal[,c(2:513)]
+rm(agilent, agilent2)
