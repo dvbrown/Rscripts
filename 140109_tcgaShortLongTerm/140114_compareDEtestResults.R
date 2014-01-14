b9170334eb392e364643d385e6f5bffd15be8ee7
dvbrown <dvbrown@student.unimelb.edu.au>
2014-01-14 12:51:14 +1100

    built data frame

--- 140109_tcgaShortLongTerm/140114_compareDEtestResults.R
+++ 140109_tcgaShortLongTerm/140114_compareDEtestResults.R
@@ -5,5 +5,21 @@ affyFiles = list.files(pattern='*.affymetrix*')
 agilentFiles = list.files(pattern='*.agilent*')
 affy = lapply(affyFiles, read.delim, header=T)
 names(affy) = affyFiles
+
 agilFiles = lapply(agilentFiles, read.delim, header=T)
 names(agilFiles) = agilentFiles
+
+# Build a dataframe of the adjusted p-values from different methods
+affyPval = cbind(affy[[1]]$adj.P.Val, affy[[2]]$adj.P.Val, affy[[3]]$adj.P.Val, affy[[4]]$adj.P.Val, affy[[5]]$adj.P.Val)
+row.names(affyPval) = affy[[1]]$ID
+colnames(affyPval) = affyFiles
+affySig = -log10(affyPval)
+
+# Build a dataframe of the adjusted p-values from different methods
+agilentPval = cbind(agilFiles[[1]]$adj.P.Val, agilFiles[[2]]$adj.P.Val, agilFiles[[3]]$adj.P.Val)
+row.names(agilentPval) = agilFiles[[1]]$ID
+colnames(agilentPval) = agilentFiles
+agilentSig = -log10(agilentPval)
+
+head(affySig)
+head(agilentSig)
