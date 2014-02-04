16e3d2b837ef741cd65944e0960f35ec591b247b
dvbrown <dvbrown@student.unimelb.edu.au>
2014-02-04 11:54:12 +1100

    plotted the MDS's

--- 131014_volcanoHeat_GLM.R
+++ 131014_volcanoHeat_GLM.R
@@ -10,8 +10,8 @@ finalResult = read.delim('140203_shortVSlong.txt')
 sigGenes=read.delim('140203_shortVSlongDEgenes.txt')
 
 require(ggplot2)
-##Highlight genes that have an absolute fold change > 2 and a p-value < Bonferroni cut-off
-finalResult$threshold = as.factor(abs(finalResult$logFC) > 2 & finalResult$FDR < 0.05)
+##Highlight genes that have an absolute fold change > 1 and a p-value < Bonferroni cut-off
+finalResult$threshold = as.factor(abs(finalResult$logFC) > 1 & finalResult$FDR < 0.1)
 
 ##Construct the volcano plot object. First get the final result by running annotate ENSEMBL IDs script with genelist
 g = ggplot(data=finalResult, aes(x=logFC, y=-log10(FDR), colour=threshold)) +
@@ -22,17 +22,18 @@ g = ggplot(data=finalResult, aes(x=logFC, y=-log10(FDR), colour=threshold)) +
   xlab("log2 fold change") + ylab("-log10 FDR adjusted p-value")
 g
 #subset gene names for only significant genes
-dd_text = finalResult[(abs(finalResult$logFC) > 5) & (finalResult$FDR < 0.0005),]
+dd_text = finalResult[(abs(finalResult$logFC) > 1) & (finalResult$FDR < 0.00001),]
 #add text to volcano
 g + geom_text(data = dd_text, aes(x=logFC, y=-log10(FDR),
-                                  label=external_gene_id, size=0.2), colour="black")
-
+                                  label=external_gene_id, size=0.2), colour="black") +
+  theme(axis.title.x = element_text(size=14)) + 
+  theme(axis.title.y = element_text(size=14))
 
 
 #Draw a heatmap. First normalise the raw counts by library size
-data = read.delim('GLMedgeR/131021_shortVSlongDEgenes.txt')
+data = read.delim('140203_shortVSlongDEgenes.txt')
 row.names(data) = data$external_gene_id
-rawData = read.delim('GLMedgeR/131021_normalisedCPM')
+rawData = read.delim('140203_normalisedCPM')
 sigGenes = as.character(data$ensembl_gene_id) #use to index the raw data
 
 sigData = as.matrix(rawData[sigGenes,])
