65b48045849b15c763dbda22935b799b82bebebc
dvbrown <dvbrown@student.unimelb.edu.au>
2014-02-20 17:00:02 +1100

    survival analysis working progress

--- 140220_TCGAsignatureAnalysisFunctions.R
+++ 140220_TCGAsignatureAnalysisFunctions.R
@@ -12,6 +12,7 @@ censorData = function(survivalData) {
     status = survivalData[,'patient.followups.followup.vitalstatus']
     status = gsub('alive', 0, status)
     status = gsub('dead', 1, status)
+    status = gsub('deceased', 1, status)
     # Check if the survival analysis can handle NAs for survival
     result = survivalData[,c(1,4,5)]
     censored = cbind(result, status)
