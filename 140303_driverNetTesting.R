858c87925b8d3fbb15ca78d4170db038211ceadc
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-10 11:52:25 +1100

    will subset the mutation matrix

--- 140303_driverNetTesting.R
+++ 140303_driverNetTesting.R
@@ -1,4 +1,5 @@
 library(DriverNet)
+library(reshape)
 
 setwd('~/Documents/public-datasets/RNA-seq/driverNet/')
 # Import data
@@ -38,3 +39,9 @@ res[1:2,]
 # The actual events is poorly documented but it appears to list for each driver gene which patient and gene is 
 # Misregulated in a list like format
 events = actualEvents(driversList)
+
+# Make the mutational matrix sparse like the TruSeq cancer amplicon
+panel = c('ABL1', 'EGFR', 'GNAS', 'MLH1', 'RET', 'AKT1', 'ERBB2', 'HNF1A', 'MPL', 'SMAD4', 'ALK', 'ERBB4', 'HRAS', 'NOTCH1', 'SMARCB1', 'APC', 'FBXW7', 'IDH1', 'NPM1',
+          'SMO', 'ATM', 'FGFR1', 'JAK2', 'NRAS', 'SRC', 'BRAF', 'FGFR2', 'JAK3', 'PDGFRA', 'STK11', 'CDH1', 'FGFR3', 'KDR', 'PIK3CA', 'TP53', 'CDKN2A', 
+          'FLT3', 'KIT', 'PTEN', 'VHL', 'CSF1R', 'GNA11', 'KRAS', 'PTPN11', 'CTNNB1', 'GNAQ', 'MET', 'RB1')	
+panel
