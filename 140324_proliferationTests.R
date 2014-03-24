12c5f4c49ff0fbd42f7f7835be4dc2aba7c4412b
dvbrown <dvbrown@student.unimelb.edu.au>
2014-03-25 10:01:35 +1100

    started again

--- 140324_proliferationTests.R
+++ 140324_proliferationTests.R
@@ -1,17 +1,19 @@
 # This script will measure the proliferation over 3 days and also compare Resazurin and LDH
 source('~/Documents/Rscripts/qPCRFunctions.R')
 
-setwd('~/Documents/Cell_biology/proliferation/Resazurin/140318_testing')
-data = read.delim('140324_data.txt', row.names=1)
+setwd('~/Documents/Cell_biology/proliferation/Resazurin/140318_testing/linear/')
+# The resazurin readings
+day1 = read.delim('140319_day1_linearReplicates.txt')
+day2 = read.delim('140320_day2_linearReplicates.txt')
+day3 = read.delim('140321_day3_linearReplicates.txt')
+resazurin = list(day1, day2, day3)
 
-# set the identifiers as factors
-data$cells = as.factor(data$cells)
-# Filter wells that had something in them
-data = data[complete.cases(data$treatment),]
+day1$cells = as.factor(day1$cells)
+day2$cells = as.factor(day2$cells)
+day3$cells = as.factor(day3$cells)
 
-data$experiment = paste(data$cells, data$treatment)
+# the ldh readings
+ldh5 = read.delim('../LDH/5min.txt')
+ldh30 = read.delim('../LDH/30min.txt')
 
-data.split = split(data, data$experiment)
-
-x = data.split[[1]]
-x = rbind(x, c(x[1,1], x[1,2], rowMeans(x[,c(3:5)]), x[6,1], x[7,1], x[8,1]))
+# Plot the replication correlations
