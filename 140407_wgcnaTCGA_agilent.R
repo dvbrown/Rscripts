9afd81919cc5c40db639081ca5cb38a411f0d053
dvbrown <dvbrown@student.unimelb.edu.au>
2014-04-15 12:59:28 +1000

    did some analysis of manual correlation

--- 140407_wgcnaTCGA_agilent.R
+++ 140407_wgcnaTCGA_agilent.R
@@ -158,14 +158,31 @@ squareAdjacency = adjacency(datExpr0,
 # Calculate the correlation between PROM1 expression and all the genes in TCGA GBM
 prom1CorrPval = corAndPvalue(x=datExpr0[,'PROM1'], y=dat)
 
-#Extract the corrlation and p-value from the returned list
-pC = prom1CorrPval$cor
-pP = prom1CorrPval$p
+#Extract the correlation and p-value from the returned list
+prom1C = prom1CorrPval$cor
+prom1Cpower = prom1C^2
 
-par(mfrow=c(2,1))
-hist(pC, main='Prom1 correlations', breaks='FD')
-hist(pP, main='Prom1 p-values', breaks='FD')
+prom1P = prom1CorrPval$p
+prom1FDR = p.adjust(pP, method='fdr')
+
+# par(mfrow=c(2,1))
+# hist(prom1Cpower, main='Prom1 correlations', breaks='FD', xlab='Weighted correlation values')
+# hist(prom1FDR, main='Prom1 p-values', breaks='FD', xlab='FDR corrected p-values')
+# par(mfrow=c(1,1))
+
+result = t(rbind(prom1C, prom1Cpower, prom1P, prom1FDR))
+colnames(result) = c('correlation', 'weighted_correlation', 'p-value', 'FDR')
+write.table(result, './wgcna/140415_standardCorrelation.txt', sep='\t')
+
+par(mfrow=c(2,2))
+hist(prom1C, main='Prom1 correlations', breaks='FD', xlab='Raw correlation values')
+hist(prom1Cpower, main='Prom1 weighted correlations', breaks='FD', xlab='R squared values')
+hist(prom1P, main='Prom1 p-values', breaks='FD', xlab='Raw p-values')
+hist(prom1FDR, main='Prom1FDR corrected p-values', breaks='FD', xlab='FDR corrected p-values')
 par(mfrow=c(1,1))
+
+qqnorm(result[,1], main='Distrubution of PROM1 correlated genes')
+qqline(result[,1])
 
 ################################################################################################################### 
 
