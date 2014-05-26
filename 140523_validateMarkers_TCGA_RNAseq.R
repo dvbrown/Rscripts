a99d7b1c0f7dd2eee3252fdcbdd010da56c4096a
dvbrown <dvbrown@student.unimelb.edu.au>
2014-05-26 16:06:57 +1000

    take out the patient overl;ap from Agilent and RNAseq

--- 140523_validateMarkers_TCGA_RNAseq.R
+++ 140523_validateMarkers_TCGA_RNAseq.R
@@ -2,5 +2,17 @@
 
 setwd('~/Documents/public-datasets/cancerBrowser/')
 
-agilent = read.delim('./TCGA_GBM_G4502A_07_2-2014-05-02/genomicMatrix')
-rnaSeq = read.delim('./TCGA_GBM_exp_HiSeqV2-2014-05-02/genomicMatrix')
+# Remove from Agilent data the RNAseq cases
+agilent = read.delim('./TCGA_GBM_G4502A_07_2-2014-05-02/genomicMatrix', row.names=1)
+rnaSeq = read.delim('./TCGA_GBM_exp_HiSeqV2-2014-05-02/genomicMatrix', row.names=1)
+
+# Measure the patient overlap between Agilent and HiSeq
+agilentNames = colnames(agilent)
+# rm(agilent)
+rnaSeqNames = colnames(rnaSeq)
+head(agilentNames)
+head(rnaSeqNames)
+duplicatedPatients = intersect(agilentNames, rnaSeqNames)
+# The number of unique genes
+168 - length(intersect(agilentNames, rnaSeqNames))
+
