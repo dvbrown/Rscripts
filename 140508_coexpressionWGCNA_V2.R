9da851830ed01a044fb11df59fc13cdeada92dc2
dvbrown <dvbrown@student.unimelb.edu.au>
2014-05-26 16:28:35 +1000

    new script to deDup AGilent

---
+++ 140508_coexpressionWGCNA_V2.R
@@ -0,0 +1,56 @@
+getwd()
+library(WGCNA)
+source('/Users/d.brown6/Documents/Rscripts/140508_coexpressionFunctions.R')
+options(stringsAsFactors=F)
+list.files()
+
+dat = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/140526_agilentDedupPatients.txt", row.names=1)
+setwd('/Users/d.brown6/Documents/public-datasets/firehose/stddata__2013_12_10/GBM/20131210_dataReformatting/dataRearranging/wgcna/manualCorrelation/')
+
+######################################## CD133 coexpressed Genes ################################################
+cd133 = correlateGeneWithGEM(dat, 'PROM1')
+#write.table(cd133, './results/140520_cd133Coexpression.txt', row.names=T, sep='\t')
+plotCoexpression(cd133, 'CD133')
+
+# subsample and return summary statistics
+# cd133SubsamplesCorr = subsample10times(dat, "PROM1", 100, "correlation")
+# cd133SubsamplesFDR = subsample10times(dat, "PROM1", 100, "FDR")
+# plotResampling(cd133SubsamplesCorr, cd133SubsamplesFDR, cd133, "PROM1")
+# rm(cd133SubsamplesCorr, cd133SubsamplesFDR)
+
+cd133genes = cd133[abs(cd133[,1]) > 2*sd(cd133[,1]) & cd133[,4] < 0.05,] # Use twice the standard deviation and significantly correlated
+cd133Square = makeSquareCoexpressionMatrix(cd133genes, dat)
+
+cd133Dissim = makeDissimilarity(cd133Square)
+
+# Make a heatmap and store the colors of the idenitfied submodules
+cd133Color = buildHeatMap(cd133Dissim, 'CD133')
+# Make MDS plot
+#pdf('cd133_MDS.pdf', paper='a4')
+makeMDS(cd133Dissim, cd133Color, 'CD133')
+#dev.off()
+
+# Export to cytoscape
+# cd133_cytoscape = cytoScapeInput(1-cd133Dissim, cd133Color,coexpressedShortList=cd133genes, 'CD133')
+
+######################################## CD44 coexpressed Genes ################################################
+cd44 = correlateGeneWithGEM(dat, 'CD44')
+# write.table(cd44, './results/140520_cd44Coexpression.txt', row.names=T, sep='\t')
+
+plotCoexpression(cd44, 'CD44')
+
+# Subset the dataframe with correlation values for those with high correlation and significance
+#cd44genes = cd44[cd44[,2] > 0.1 & cd44[,4] < 0.05,]
+cd44genes = cd44[abs(cd44[,1]) > 2*sd(cd44[,1]) & cd44[,4] < 0.05,] # Use twice the standard deviation and significantly correlated
+cd44Square = makeSquareCoexpressionMatrix(cd44genes, dat)
+
+cd44Dissim = makeDissimilarity(cd44Square)
+
+# Make a heatmap and store the colors of the idenitfied submodules
+cd44Color = buildHeatMap(cd44Dissim, 'CD44')
+# Make MDS plot
+#pdf('cd44_MDS.pdf', paper='a4')
+makeMDS(cd44Dissim, cd44Color, 'CD44')
+#dev.off()
+
+cd44_cytoscape = cytoScapeInput(1-cd44Dissim, cd44Color, coexpressedShortList=cd44genes, 'CD44')
--- stemSignaturesManuscript/src/140526_deDupAgilentFromRNAseq.R
+++ stemSignaturesManuscript/src/140526_deDupAgilentFromRNAseq.R
@@ -1,18 +1,13 @@
-# Classify the RNA-seq TCGA samples provide they are different patients than Agilent
-
-setwd('~/Documents/public-datasets/cancerBrowser/')
-
-# Remove from Agilent data the RNAseq cases
 agilent = read.delim('./TCGA_GBM_G4502A_07_2-2014-05-02/genomicMatrix', row.names=1)
 rnaSeq = read.delim('./TCGA_GBM_exp_HiSeqV2-2014-05-02/genomicMatrix', row.names=1)
 
 # Measure the patient overlap between Agilent and HiSeq
 agilentNames = colnames(agilent)
-# rm(agilent)
 rnaSeqNames = colnames(rnaSeq)
-head(agilentNames)
-head(rnaSeqNames)
 duplicatedPatients = intersect(agilentNames, rnaSeqNames)
-# The number of unique genes
-168 - length(intersect(agilentNames, rnaSeqNames))
-
+uniquePatients = setdiff(agilentNames, rnaSeqNames)
+# Subset out the duplicates
+agilentDeDup = t(agilent)
+# Return the unique cases to Agilent
+agilentDeDup1 = t(agilentDeDup[uniquePatients,])
+write.table(agilentDeDup1, file="140526_agilentDedupPatients.txt", sep="\t")
