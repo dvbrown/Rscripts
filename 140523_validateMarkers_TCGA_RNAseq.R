8feb70404dbbd881e33ce8de54fa5b9cba17f806
dvbrown <dvbrown@student.unimelb.edu.au>
2014-05-28 09:16:29 +1000

    annotate the script

--- 140523_validateMarkers_TCGA_RNAseq.R
+++ 140523_validateMarkers_TCGA_RNAseq.R
@@ -34,6 +34,10 @@ sigs = list("CD133" = row.names(cd133Sig), "CD44" = row.names(cd44Sig))
 result = gsva(rnaseqM, sigs,  rnaseq=F, verbose=T, parallel.sz=1)
 result = t(result$es.obs)
 
+# Compare ssGSEA
+# result = gsva(rnaseqM, sigs,  rnaseq=F, verbose=T, parallel.sz=1, method="ssGSEA")
+# result = t(result)
+
 par(mfrow=c(2,1))
 hist(result[,1], breaks='FD', main="CD133 enrichment score distribution", xlim=c(0,1))
 hist(result[,2], breaks='FD', main="CD44 enrichment score distribution", xlim=c(0,1))
@@ -59,23 +63,24 @@ length(cd44Subtype)
 ############################################## Examine the Verhaak molecular subtypes #############################################
 clinical = read.delim("~/Documents/public-datasets/cancerBrowser/TCGA_GBM_exp_HiSeqV2-2014-05-02/clinical_dataDots.txt", row.names=1)
 
+# Extract the clinical data for the RNAseq patients
 matched = intersect(row.names(clinical), colnames(rnaseq))
-
+# Subset clinical data for intersect
 clin = clinical[matched, c("CDE_DxAge", "CDE_survival_time", "CDE_vital_status",
                    "G_CIMP_STATUS","GeneExp_Subtype", "X_EVENT","days_to_tumor_progression", "gender")]
 
+# Merge RNAseq - FACs data and clinicial data. Add Verhaak subtype
 verhaakSubtype = bindGeneExprClinical(clin, result1)
 verhaakSubtype = sort.dataframe(verhaakSubtype, 4)
 
-subTypeHeat = cbind(verhaakSubtype$CD133, verhaakSubtype$CD44)
-write.table(subTypeHeat, "output.txt", sep='\t')
-subTypeHeat = read.delim("output.txt", row.names=1)
-subTypeHeat = as.matrix(subTypeHeat)
+# The damn datatypes are not correct. Dump and read in object from file
+write.table(verhaakSubtype, "output.txt", sep='\t')
+verhaakSubtype = read.delim("output.txt", row.names=1)
 
-heatmap.2(t(subTypeHeat), cexRow=1.5, main="ssGSEA FACS markers based on Subtype", Colv=verhaakSubtype$colours,
-          keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="none", ColSideColors=verhaakSubtype$colours,
+subTypeHeat = as.matrix(verhaakSubtype[,c("CD133","CD44")])
+
+# Make heat map with Veerhaak subtype
+heatmap.2(t(subTypeHeat), cexRow=1.5, main="Enrichment of FACS marker signatures \n in Molecular Subtype", Colv=verhaakSubtype$colours,
+          keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="none", ColSideColors=as.character(verhaakSubtype$colours),
           labRow=c("CD133", "CD44"), xlab="Samples", labCol=NA, offsetRow=c(1,1), margins=c(2,7))
 
-heatmap.2(t(subTypeHeat), cexRow=2, main="ssGSEA FACS markers based on Subtype", #Colv=verhaakSubtype$colours,
-          keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", ColSideColors=verhaakSubtype$colours,
-          labRow=c("CD133", "CD44"), xlab="Samples", labCol=NA, offsetRow=c(1,1))
