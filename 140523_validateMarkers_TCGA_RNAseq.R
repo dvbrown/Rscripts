81b000e9abe89b0f867ba49fab37b4a437a2b702
dvbrown <dvbrown@student.unimelb.edu.au>
2014-05-27 12:39:04 +1000

    got ssGSEA to work. should I scale?

--- 140523_validateMarkers_TCGA_RNAseq.R
+++ 140523_validateMarkers_TCGA_RNAseq.R
@@ -1,5 +1,6 @@
 # Classify the RNA-seq TCGA samples provide they are different patients than Agilent
 library(GSVA)
+library(gplots)
 
 setwd('~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/')
 
@@ -9,6 +10,15 @@ cd44Sig = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/res
 
 # Mung data into form for GSVA
 rnaseqM = as.matrix(rnaseq)
-sigs = list("CD133" = cd133Sig, "CD44" = cd44Sig)
+sigs = list("CD133" = row.names(cd133Sig), "CD44" = row.names(cd44Sig))
 
 result = gsva(rnaseqM, sigs, method="ssgsea", rnaseq=T, verbose=T)
+result = t(result)
+
+par(mfrow=c(2,1))
+hist(result[,1], breaks='FD', main="CD133", xlim=c(0,1))
+hist(result[,2], breaks='FD', main="CD44", xlim=c(0,1))
+par(mfrow=c(1,1))
+
+heatmap.2(result, Colv=NA, cexRow=0.5, cexCol=0.9, main="ssGSEA FACS markers", scale="column", keysize=1, trace="none")
+heatmap.2(result, Colv=NA, cexRow=0.5, cexCol=0.9, main="ssGSEA FACS markers", keysize=1, trace="none")
