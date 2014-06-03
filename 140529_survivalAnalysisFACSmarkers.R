9b711e373277434586b35fe132855c8eb8fa7207
dvbrown <dvbrown@student.unimelb.edu.au>
2014-06-03 14:24:38 +1000

    tried out intermediate subtype for Agilent
    It didn’t work

--- 140529_survivalAnalysisFACSmarkers.R
+++ 140529_survivalAnalysisFACSmarkers.R
@@ -87,10 +87,13 @@ test
 text(locator(1),labels='p=0.152', cex=1) #add the p-value to the graph
 
 ############################################# Survival curve for subtype ##################################
-#generate the survival object and plot a Kaplan-Meier
-data.surv = Surv(boundData$CDE_survival_time, event=boundData$X_EVENT)
+# Remove nosubtype cases
+boundDataSub = boundData[!boundData$GeneExp_Subtype == "",]
 
-sur.fit = survfit(data.surv~GeneExp_Subtype, boundData)
+#generate the survival object and plot a Kaplan-Meier
+data.surv = Surv(boundDataSub$CDE_survival_time, event=boundDataSub$X_EVENT)
+
+sur.fit = survfit(data.surv~GeneExp_Subtype, boundDataSub)
 
 plot(sur.fit, main='FACS marker coexpression signature in \nGlioblastoma multiforme by RNAseq',ylab='Survival probability',xlab='survival (days)', 
      col=c("red",'green','orange', "blue"),xlim=c(0,750), cex=1.75)
@@ -99,6 +102,93 @@ legend('topright', c('Proneural', 'Neural', 'Mesenchymal',"Classical"),
        col=c("red",'green','orange', "blue"),lwd=1, cex=0.9, bty='n', xjust=0.5, yjust=0.5)
 summary(data.surv)
 #test for a difference between curves
-test = survdiff(data.surv~boundData$GeneExp_Subtype, subset=boundData$GeneExp_Subtype %in% c("Proneural", "Mesenchymal"))
+test = survdiff(data.surv~boundDataSub$GeneExp_Subtype, subset=boundDataSub$GeneExp_Subtype %in% c("Proneural", "Mesenchymal"))
 test
-# text(locator(1),labels='p=0.071', cex=1) #add the p-value to the graph
+
+
+
+
+
+
+
+############################################# Test the Agilent dataset ##################################
+callMarkerSubtype <- function (signatureScore, CD133cutoff, CD44cutoff) {
+    # Takes a dataframe containing the signature scores and adds a new column that calls FACS marker subtype
+    signatureScore$subtype = ""
+    signatureScore$subtype = ifelse(signatureScore[,"CD133"] > signatureScore[,"CD44"], "CD133", "CD44")
+    # Not having and intermediate case is also better for the Kaplan Myer curve
+    signatureScore$subtype[signatureScore[,"CD133"] < -CD133cutoff & signatureScore[,"CD44"] < CD44cutoff] = "intermediate"
+    signatureScore = sort.dataframe(signatureScore, "subtype")
+    signatureScore$subtype = as.factor(signatureScore$subtype)
+    return (signatureScore)
+}
+
+# The signature derived from Agilent array
+verhaakSigAgi = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/survival/140603_verhaakSubtypeAgilent.txt", row.names=1)
+
+clinical = read.delim("~/Documents/public-datasets/cancerBrowser/TCGA_GBM_G4502A_07_2-2014-05-02/clinical_dataDots.txt", row.names=1)
+
+verhaakSignature = verhaakSigAgi[,c("CD133","CD44")]
+
+verhaakSubtypeCall = callMarkerSubtype(verhaakSignature, 0, 0)
+# Extract the clinical data for the Agilent patients
+matched = intersect(row.names(clinical), row.names(verhaakSubtypeCall))
+# Subset clinical data for intersect
+clin = clinical[matched, c("CDE_DxAge", "CDE_survival_time", "CDE_vital_status","X_EVENT", "gender", "G_CIMP_STATUS", "GeneExp_Subtype")]
+
+
+# Make an object to plot density gram
+lattPlot = data.frame(as.numeric(c(verhaakSignature[,"CD133"], verhaakSignature[,"CD44"])), 
+                      c(rep("CD133", nrow(verhaakSignature)), rep("CD44", nrow(verhaakSignature))))
+colnames(lattPlot) = c('value', "signature")
+
+ggplot(lattPlot, aes(value, fill = signature)) + geom_density(alpha = 0.2) +
+    xlab("Signature score") + ylab("Density") + # Set axis labels
+    ggtitle("Distribution of CD133 and \nCD44 signature scores in Agilent") +  # Set title
+    coord_cartesian(xlim = c(-1, 1)) + theme_bw(base_size=20) + geom_vline(xintercept=0.05, colour="red") # The 0.125 is where I will call indeterminate
+
+
+boundData = merge.data.frame(clin, verhaakSubtypeCall, by.x="row.names", by.y="row.names")
+boundData = sort.dataframe(boundData, "subtype")
+row.names(boundData) = boundData$Row.names
+boundData$subtype = as.factor(boundData$subtype)
+boundData$gender = as.factor(boundData$gender)
+
+#generate the survival object and plot a Kaplan-Meier
+data.surv = Surv(boundData$CDE_survival_time, event=boundData$X_EVENT)
+
+sur.fit = survfit(data.surv~subtype, boundData)
+
+plot(sur.fit, main='FACS marker coexpression signature in \nGlioblastoma multiforme by Agilent array',ylab='Survival probability',xlab='survival (days)', 
+     col=c("red",'blue'),#'green'),
+     xlim=c(0,750), cex=1.75, conf.int=F, lwd=1.5)
+
+legend('topright', c('CD133', 'CD44'),# 'Intermediate'), 
+       col=c("red",'blue'),#'green'),
+       lwd=2, cex=1.2, bty='n', xjust=0.5, yjust=0.5)
+
+summary(data.surv)
+#test for a difference between curves
+test = survdiff(data.surv~boundData$subtype)#, subset=!boundData$subtype %in% "intermediate")
+test
+text(locator(1),labels='p=0.591', cex=1) #add the p-value to the graph
+
+
+############################################# Survival curve for subtype ##################################
+# Remove nosubtype cases
+boundDataSub = boundData[!boundData$GeneExp_Subtype == "",]
+
+#generate the survival object and plot a Kaplan-Meier
+data.surv = Surv(boundDataSub$CDE_survival_time, event=boundDataSub$X_EVENT)
+
+sur.fit = survfit(data.surv~GeneExp_Subtype, boundDataSub)
+
+plot(sur.fit, main='FACS marker coexpression signature in \nGlioblastoma multiforme by Agilent array',ylab='Survival probability',xlab='survival (days)', 
+     col=c("red",'green','orange', "blue"),xlim=c(0,750), cex=1.75)
+
+legend('topright', c('Proneural', 'Neural', 'Mesenchymal',"Classical"), 
+       col=c("red",'green','orange', "blue"),lwd=1, cex=0.9, bty='n', xjust=0.5, yjust=0.5)
+summary(data.surv)
+#test for a difference between curves
+test = survdiff(data.surv~boundDataSub$GeneExp_Subtype, subset=boundDataSub$GeneExp_Subtype %in% c("Proneural", "Mesenchymal"))
+test
--- 140529_validateMarkers_TCGA_RNAseqV2.R
+++ 140529_validateMarkers_TCGA_RNAseqV2.R
@@ -8,7 +8,7 @@ source("~/Documents/Rscripts/140508_coexpressionFunctions.R")
 ############################################# IO ##################################################################
 setwd('~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/')
 rnaseq = read.delim("~/Documents/public-datasets/cancerBrowser/TCGA_GBM_exp_HiSeqV2-2014-05-02/genomicMatrix", row.names=1)
-agilent = read.delim('~/Documents/public-datasets/cancerBrowser/deDupAgilent/140526_agilentDedupPatients.txt', row.names=1)
+# agilent = read.delim('~/Documents/public-datasets/cancerBrowser/deDupAgilent/140526_agilentDedupPatients.txt', row.names=1)
 tcgaSigs = read.delim('~/Documents/public-datasets/TCGA/classficationSignature/131022_danFixedTCGAsignature.txt')
 
 clinical = read.delim("~/Documents/public-datasets/cancerBrowser/TCGA_GBM_exp_HiSeqV2-2014-05-02/clinical_dataDots.txt", row.names=1)
@@ -161,7 +161,7 @@ agilentM[is.na(agilentM)] <- 0
 bigSigs = list("CD133" = row.names(cd133Sig), "CD44" = row.names(cd44Sig), "CD15" = row.names(cd15),
                "ALDH1"=row.names(aldh1), "ITGA6"=row.names(itag6), "L1CAM"=row.names(l1cam))
 
-# Extract the clinical data for the RNAseq patients
+# Extract the clinical data for the Agilent patients
 matched = intersect(row.names(clinical), colnames(agilentM))
 # Subset clinical data for intersect
 clin = clinical[matched, c("CDE_DxAge", "CDE_survival_time", "CDE_vital_status",
@@ -171,13 +171,15 @@ clin = clinical[matched, c("CDE_DxAge", "CDE_survival_time", "CDE_vital_status",
 bigResult = gsva(agilentM, bigSigs,  rnaseq=F, verbose=T, parallel.sz=1)
 bigResult = t(bigResult$es.obs)
 
-# Merge RNAseq - FACs data and clinicial data. Add Verhaak subtype
+# Merge Agilent - FACs data and clinicial data. Add Verhaak subtype
 signatures = names(bigSigs)
-verhaakSubtype = bindGeneExprClinical(clin, bigResult, signatures)
+verhaakSubtype = bindGeneExprCIMPClinical(clin, bigResult, signatures)
 verhaakSubtype = sort.dataframe(verhaakSubtype, 'colours')
 
 # The damn datatypes are not correct. Dump and read in object from file
 write.table(verhaakSubtype, "output.txt", sep='\t')
+# write.table(verhaakSubtype, "./survival/140603_verhaakSubtypeAgilent.txt", sep='\t')
+
 verhaakSubtype = read.delim("output.txt", row.names=1)
 
 subTypeHeat = as.matrix(verhaakSubtype[,signatures])
@@ -185,5 +187,5 @@ subTypeHeat = as.matrix(verhaakSubtype[,signatures])
 # Make heat map with Veerhaak subtype
 heatmap.2(t(subTypeHeat), cexRow=1.5, main="Enrichment of FACS marker signatures \n in Molecular Subtype", 
           Colv=verhaakSubtype$colours, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
-          ColSideColors=as.character(verhaakSubtype$colours), labRow=colnames(subTypeHeat), xlab="Samples", labCol=NA, 
-          offsetRow=c(1,1), margins=c(2,7.5))
+          ColSideColors=as.character(verhaakSubtype$colours), labRow=colnames(subTypeHeat), xlab="Aglent samples", labCol=NA, 
+          offsetRow=c(1,1), margins=c(2,7.5), ylab="Marker")
