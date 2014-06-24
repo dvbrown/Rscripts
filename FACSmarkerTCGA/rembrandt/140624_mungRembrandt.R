27d26d4d523035d4dd2250259a326f95b7616ff3
dvbrown <dvbrown@student.unimelb.edu.au>
2014-06-24 14:28:14 +1000

    commit before I run out of memory

--- FACSmarkerTCGA/rembrandt/140624_mungRembrandt.R
+++ FACSmarkerTCGA/rembrandt/140624_mungRembrandt.R
@@ -1,7 +1,12 @@
-library(affy)
+library(affyPLM)
 library(limma)
 
 setwd('~/Documents/public-datasets/rembrandt/rembrandt_GBM/')
-list.files()
+allFiles = list.files()
+allFiles 
 
-rawData = ReadAffy()
+# rawData = ReadAffy('00518392_U133P2.CEL', rm.mask=T, rm.outliers=T)
+# exprData = exprs(rawData)
+# rmaData = justRMA('00518392_U133P2.CEL')
+
+rmaData = just(allFiles, rm.outliers=T, rm.mask=T, verbose=T, sampleNames=allFiles, destructive=T)
