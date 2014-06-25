0371dc0d25dc42cb00d4b1248296df345f4e0232
dvbrown <dvbrown@student.unimelb.edu.au>
2014-06-25 11:30:45 +1000

    use of ddply to take means

--- FACSmarkerTCGA/rembrandt/140624_mungRembrandt.R
+++ FACSmarkerTCGA/rembrandt/140624_mungRembrandt.R
@@ -3,6 +3,7 @@ library(limma)
 library(sqldf)
 library(annotate)
 library(hgu133plus2.db)
+library(plyr)
 
 source('~/Documents/Rscripts/120704-sortDataFrame.R')
 setwd('~/Documents/public-datasets/rembrandt/rembrandt_GBM/')
@@ -17,14 +18,14 @@ allFiles
 #normalisedData = as.data.frame(exprs(rmaData))
 
 # Open a connection to Test.sqlite database
-# setwd('~/Documents/public-datasets/rembrandt/rembrandt_GBM/processedData/')
-# db <- dbConnect(SQLite(), dbname="140624_rembrandtGBM.sqlite")
+setwd('~/Documents/public-datasets/rembrandt/rembrandt_GBM/processedData/')
+db <- dbConnect(SQLite(), dbname="./140624_rembrandtGBM.sqlite")
 # dbWriteTable(conn = db, name = "rembrandtRMA", value = normalisedData, row.names = TRUE)
 # dbDisconnect(db)
+normalisedData = dbReadTable(conn = db, name = "rembrandtRMA", row.names = 'row_names')
 
 boxData = normalisedData[,150:200]
 boxplot(boxData, col=rainbow(50), main='Rembrandt RMA normalised', par(las=2))
-
 head(normalisedData)
 
 ################# Extract the probe ID mappings with the expression data #########
@@ -47,18 +48,27 @@ head(genelist)
 annotatedData = merge(normalisedData, genelist, by.x='row.names', by.y='GeneID')
 
 ################# Calculate row median then take the highest probe intensity #########
-rowMed = apply(normalisedData, 1, median)
-annotatedData$rowMed = rowMed
-annotateSort = sort.dataframe(annotatedData, 232)
-
+# rowMed = apply(normalisedData, 1, median)
+# annotatedData$rowMed = rowMed
+# annotateSort = sort.dataframe(annotatedData, 232)
 # Remove NA and duplicated geneIDs
-annotateSort <- annotateSort[!is.na(annotateSort$GeneSymbol), ]
-annotateSort <- annotateSort[!duplicated(annotateSort$GeneSymbol), ]
-head(annotateSort)
+# annotateSort <- annotateSort[!is.na(annotateSort$GeneSymbol), ]
+# annotateSort <- annotateSort[!duplicated(annotateSort$GeneSymbol), ]
+# head(annotateSort)
 
-dbWriteTable(conn = db, name = "rembrandtSummarised", value = annotateSort, row.names = TRUE)
+################# Calculate the mean of duplicated probes and then take that value #########
+rowMean = rowMeans(annotatedData[,2:229])
+annotatedData$rowMean = rowMean
+#extract the duplicated probes/genes
+dupProbes = annotatedData[duplicated(annotatedData$GeneSymbol), ]
+uniqueProbes = annotatedData[!duplicated(annotatedData$GeneSymbol), ]
 
-write.table(annotateSort, './140624_rembrandtGBM_RMAsummarise.txt', sep='\t')
+dataDup.mean = ddply(annotatedData[,2:230], 'GeneSymbol', numcolwise(mean))
+
+dataDeDup = rbind(dataDup.mean, uniqueProbes[,2:230])
+
+#dbWriteTable(conn = db, name = "rembrandtSummarised", value = annotateSort, row.names = TRUE)
+#write.table(annotateSort, './140624_rembrandtGBM_RMAsummarise.txt', sep='\t')
 
 ################# Write out the clinical Data ########################## 
 clinicalData = read.csv('~/Documents/public-datasets/rembrandt/REMBRANDT_survivalinmonths.csv')
