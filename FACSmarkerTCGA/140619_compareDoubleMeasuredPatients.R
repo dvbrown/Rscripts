0d07f39b7b059344d73c210e5d9bf51bc45a4d4f
dvbrown <dvbro0@gmail.com>
2014-06-30 20:37:42 +1000

    fisher exact tests

--- FACSmarkerTCGA/140619_compareDoubleMeasuredPatients.R
+++ FACSmarkerTCGA/140619_compareDoubleMeasuredPatients.R
@@ -72,8 +72,17 @@ gCimp = xtabs(~ G_CIMP_STATUS + platform, data= compClinical, exclude="")
 summary(gCimp)
 
 # Set up multiple fisher tests to investigate clinical parameters
-fisher.test(xtabs(~ CDE_alk_chemoradiation_standard + platform, data=compClinical))
-# More...
+fisher.test(xtabs(~ CDE_alk_chemoradiation_standard + platform, data=compClinical))# N.S
+fisher.test(xtabs(~ CDE_chemo_tmz + platform, data=compClinical)) # 0.011
+fisher.test(xtabs(~ CDE_chemo_tmz_long + platform, data=compClinical)) # N.S
+fisher.test(xtabs(~ CDE_radiation_standard + platform, data=compClinical)) # N.S
+fisher.test(xtabs(~ CDE_radiation_adjuvant + platform, data=compClinical)) # N.S
+fisher.test(xtabs(~ CDE_radiation_adjuvant_standard + platform, data=compClinical)) # NS
+t.test(CDE_chemo_alk_days ~ platform, data=compClinical) # N.S
+
+fisher.test(xtabs(~ CDE_chemo_adjuvant_tmz + platform, data=compClinical)) # Highly significant    
+fisher.test(xtabs(~ CDE_chemo_adjuvant_alk + platform, data=compClinical)) # almost significant    
+
 
 ################ Mung the GEMs into shape for GSVA #############################
 
