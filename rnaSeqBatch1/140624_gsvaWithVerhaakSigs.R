f17aeb6509184943f679c7f0e3c1182480e94741
dvbrown <dvbrown@student.unimelb.edu.au>
2014-07-01 15:29:41 +1000

    made heat map for batch 1 with CD133/ CD44 signature

--- rnaSeqBatch1/140624_gsvaWithVerhaakSigs.R
+++ rnaSeqBatch1/140624_gsvaWithVerhaakSigs.R
@@ -33,7 +33,7 @@ result.corrected = gsva(data.corrected, verhaakSigMung)
 result.corrected = t(result.corrected$es.obs)
 
 result.log = gsva(data.log, verhaakSigMung)
-result.log = t(result.log$es.obs)
+result.log = result.log$es.obs
 
 myPalette <- colorRampPalette(c("blue", "white", "yellow"))(n = 1000)
 
@@ -46,3 +46,34 @@ heatmap.2(result.corrected, cexRow=1.5, main="Enrichment of Verhaak signatures \
 heatmap.2(result.log, cexRow=1.5, main="Enrichment of Verhaak signatures \n in RNAseq batch 1", 
           keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", Colv=NULL,
           labCol=colnames(result.log), xlab="RNAseq batch 1 log2 cpm", labRow=row.names(result.log), offsetRow=c(1,1), margins=c(15,10))
+
+
+################################# Enrich against my CD133/ CD44 signature ######################################
+cd133Sig = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/140527_cd133Cutoff.txt", row.names=1)
+cd44Sig = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/140527_cd44Cutoff.txt", row.names=1)
+cd15 = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/CD15/140528_cd15Cutoff.txt", row.names=1)
+aldh1 = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/ALDH1/140528_ALDH1Cutoff.txt", row.names=1)
+itag6 = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/ITGA6//140528_ITGA6Cutoff.txt", row.names=1)
+l1cam = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/L1CAM/140528_L1CAMCutoff.txt", row.names=1)
+
+bigSigs = list("CD133" = row.names(cd133Sig), "CD44" = row.names(cd44Sig), "CD15" = row.names(cd15))
+
+result.log = gsva(data.log, bigSigs, verbose=T, parallel.sz=1)
+result.log = (result.log$es.obs)
+
+# Make heat map with subtype
+heatmap.2(result.log, cexRow=1.5, main="Enrichment of CD133/ CD44 signatures \n in RNAseq batch 1", 
+          keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", Colv=NULL,
+          labCol=colnames(result.log), xlab="RNAseq batch 1 log2 cpm", labRow=row.names(result.log), offsetRow=c(1,1), margins=c(15,10))
+
+
+biggerSigs = list("CD133" = row.names(cd133Sig), "CD44" = row.names(cd44Sig), "CD15" = row.names(cd15),
+                  'Mesenchymal'=verhaakSigMung$Mesenchymal, 'Proneural'= verhaakSigMung$Proneural)
+
+resultBig.log = gsva(data.log, biggerSigs, verbose=T, parallel.sz=1)
+resultBig.log = resultBig.log$es.obs
+
+# Make heat map with subtype
+heatmap.2(resultBig.log, cexRow=1.5, main="Enrichment of signatures \n in RNAseq batch 1", 
+          keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", Colv=NULL,
+          labCol=colnames(resultBig.log), xlab="RNAseq batch 1 log2 cpm", labRow=row.names(resultBig.log), offsetRow=c(1,1), margins=c(15,10))
