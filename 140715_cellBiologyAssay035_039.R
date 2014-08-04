95e3588ebeba57f20f8abf8533f1c72f92f93c49
dvbrown <dvbrown@student.unimelb.edu.au>
2014-08-04 11:28:16 +1000

    reduce scrpit

---
+++ 140715_cellBiologyAssay035_039.R
@@ -0,0 +1,7 @@
+source('~/Documents/Rscripts/cellBiologyAnalysisFunctions.R')
+
+############################################## IO ###############################################
+setwd('~/Documents/Cell_biology/proliferation/Resazurin/140710_039_035/')
+growthD3 = read.delim('140709_day3Rep.txt')
+growthD7 = read.delim('140714_day7Rep.txt')
+
