0df6fcebc8a931ba15ae224a1abde379fe2bf37a
dvbrown <dvbro0@gmail.com>
2014-08-13 20:39:06 +1000

    discard this

--- 140715_cellBiologyAssay035_039.R
+++ 140715_cellBiologyAssay035_039.R
@@ -1,5 +1,4 @@
-source('~/Documents/Rscripts/140211_multiplotGgplot2.R')
-source('~/Documents/Rscripts/140211_multiplotGgplot2.R')
+source('140211_multiplotGgplot2.R')
 
 backgroundMeanSD <- function (dataFrame) {
     # Take the dataframe of raw data and then remove background fluorescence and take the mean and sd
@@ -47,5 +46,17 @@ extractPosNegReplicates = function(dataFrame) {
 
 ############################################## Read in the resazurin assay readings ###############################################
 setwd('~/Documents/Cell_biology/proliferation/Resazurin/140710_039_035/')
+setwd('~/Documents/')
 growthD3 = read.delim('140709_day3Rep.txt')
 growthD7 = read.delim('140714_day7Rep.txt')
+
+# Subtract background and take mean and SD
+day3Growth = backgroundMeanSD(growthD3)
+day7Growth = backgroundMeanSD(growthD7)
+
+#plot replicates
+par(mfrow=c(2,1))
+plot(growthD7$rep1, growthD7$rep2, ylab='replicate 2', xlab='replicate1', main='Consistency day7', pch=16)
+abline(lm(growthD7$rep1~growthD7$rep2), col='red')
+plot(growthD3$rep1, growthD3$rep2, ylab='replicate 2', xlab='replicate1', main='Consistency day3', pch=16)
+abline(lm(growthD3$rep1~growthD3$rep2), col='red')
