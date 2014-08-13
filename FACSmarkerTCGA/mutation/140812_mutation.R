5e0a4028836a0403c806154d3de3233ae715870c
dvbrown <dvbrown@student.unimelb.edu.au>
2014-08-13 16:55:57 +1000

    getting the data munged

--- FACSmarkerTCGA/mutation/140812_mutation.R
+++ FACSmarkerTCGA/mutation/140812_mutation.R
@@ -1,7 +1,19 @@
-library(jsonlite)
+# This was downloaded from the cancer browser which in turn derives from the cancer genome atlas
+library(gtools)
 
 setwd('~/Documents/public-datasets/cancerBrowser/TCGA_GBM_mutation-2014-05-02/')
-rawData = read.delim('genomicMatrix', row.names=1)
-rawData[1,1] = 'blank'
+rawData = read.delim('genomicMatrix')
+# Start with the RNAseq patitents but I have to write the Agilent patients to file
+patientSubtype = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/survival/survivalTables/140606_RNAseq_SurvivalBoundData.txt", row.names=1)
+#rawData[1,1] = 'blank'
 
-row.names(rawData) = rawData$sample
+# Remove cases for which there has been no gene assigned
+genes = rawData[,1]
+rawData = rawData[,c(2:292)]
+cases = complete.cases(genes)
+data = rawData[cases,]
+row.names(data) = genes[cases]
+
+# Subset the subtyped Patietns that exist for the mutations
+matched = intersect(row.names(patientSubtype), colnames(data))
+patientMutation = patientSubtype[matched,]
