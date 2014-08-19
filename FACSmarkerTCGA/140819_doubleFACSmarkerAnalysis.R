a60ec15f43639152bd42f24b5c48ddc373690782
dvbrown <dvbrown@student.unimelb.edu.au>
2014-08-19 17:40:22 +1000

    new qPCR script for new data

--- FACSmarkerTCGA/140819_doubleFACSmarkerAnalysis.R
+++ FACSmarkerTCGA/140819_doubleFACSmarkerAnalysis.R
@@ -1,8 +1,8 @@
 getwd()
 library(WGCNA)
 library(ggplot2)
-source('/Users/d.brown6/Documents/Rscripts/140508_coexpressionFunctions.R')
-options(stringsAsFactors=F)
+source('/Users/d.brown6/Documents/Rscripts/FACSmarkerTCGA/140508_coexpressionFunctions.R')
+source('/Users/d.brown6/Documents/Rscripts/multiplot.R')
 list.files()
 
 dat = read.delim("~/Documents/public-datasets/cancerBrowser/deDupAgilent/140526_agilentDedupPatients.txt", row.names=1)
@@ -40,3 +40,43 @@ plotCoexpression(cd15, 'CD15')
 # Subset the dataframe with correlation values for those with high correlation and significance
 # Use twice the standard deviation and significantly correlated
 cd15genes = cd15[cd15[,1] > 3*sd(cd15[,1]) & cd15[,4] < 0.05,]
+
+cd15Square = makeSquareCoexpressionMatrix(cd15genes, dat)
+cd15Dissim = makeDissimilarity(cd15Square)
+
+######################################## Bind correlation and FDR ################################################
+correlations = cbind(cd133[,1], cd44[,1], cd15[,1])
+fdrs = cbind(cd133[,4], cd44[,4], cd15[,4])
+colnames(correlations) = c('CD133', 'CD44', 'CD15')
+colnames(fdrs) = c('CD133', 'CD44', 'CD15')
+correlations = as.data.frame(correlations)
+
+# get the genes which have been thresholded
+correlated = c(row.names(cd133genes), row.names(cd44genes), row.names(cd15genes))
+correlated = unique(correlated)
+
+correlations$threshold = "not significant"
+correlations[correlated,]$threshold = "significant"
+
+
+######################################## Plot the doubles ################################################
+
+cd133_44 = ggplot(data=correlations, aes(x=CD133, y=CD44, color=threshold)) + 
+            geom_point(shape=19, alpha=1/4) + geom_smooth(method=lm, colour='red') +
+            xlab("CD133") + ylab("CD44") + # Set axis labels
+            ggtitle("Correlation of genes with double FACS markers") +  # Set title
+            theme_bw(base_size=18)
+
+cd133_15 = ggplot(data=correlations, aes(x=CD133, y=CD15, color=threshold)) + 
+    geom_point(shape=19, alpha=1/4) + geom_smooth(method=lm, colour='red') +
+    xlab("CD133") + ylab("CD15") + # Set axis labels
+    ggtitle("Correlation of genes with double FACS markers") +  # Set title
+    theme_bw(base_size=18)
+
+cd44_15 = ggplot(data=correlations, aes(x=CD44, y=CD15, color=threshold)) + 
+    geom_point(shape=19, alpha=1/4) + geom_smooth(method=lm, colour='red') +
+    xlab("CD44") + ylab("CD15") + # Set axis labels
+    ggtitle("Correlation of genes with double FACS markers") +  # Set title
+    theme_bw(base_size=18)
+
+multiplot(cd133_15, cd133_44, cd44_15, cols=2)
