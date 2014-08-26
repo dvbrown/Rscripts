bacb6206e96c70f4e90b440117448644fa27a871
dvbrown <dvbrown@student.unimelb.edu.au>
2014-08-26 11:56:01 +1000

    entered design matrix

--- publicGPSC/140825_bhat2013.R
+++ publicGPSC/140825_bhat2013.R
@@ -2,5 +2,13 @@ library(limma)
 library(affy)
 
 setwd('~/Documents/public-datasets/GPSC_subgroups/bhat2013/GSE49009_RAW/')
+list.files()
 
 rawData = ReadAffy()
+dm = readTargets('../designMatrix.txt')
+
+par(mfrow=(c(2,1)))
+boxplot(rawData, col=rainbow(21), main='Bhat unNormalised data', las=2, mar=c(5,5,5,2))
+# data = rma(rawData, verbose=T)
+# norm = exprs(data)
+boxplot(norm, col=rainbow(21), main='Bhat normalised data', las=2, mar=c(5,5,5,2))
