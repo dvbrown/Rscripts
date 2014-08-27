6cb4192a2e1e6e6433a06fca2c72d28c4249ff9c
dvbrown <dvbrown@student.unimelb.edu.au>
2014-08-27 12:50:58 +1000

    made various heatmaps

--- publicGPSC/140826_bhatSignatures.R
+++ publicGPSC/140826_bhatSignatures.R
@@ -29,7 +29,22 @@ dm$colour[dm$Subtype %in% 'Proneural'] = 'purple'
 dm$colour[dm$Subtype %in% 'Mesenchymal'] = 'red'
 myPalette <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
 
+##################### heatMaps ###########################
+
 heatmap.2(t(bigResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in Molecular Subtype", scale="none",
           Rowv=NULL, Colv=TRUE, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", 
-          ColSideColors=as.character(dm$colour), xlab="Aglent samples", labCol=row.names(bigResult), labRow=colnames(bigResult), 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=row.names(bigResult), labRow=colnames(bigResult), 
+          offsetRow=c(1,1), margins=c(8,7))
+
+smallResult = bigResult[,c(1:3)]
+heatmap.2(t(smallResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in Molecular Subtype", scale="none",
+          Rowv=NULL, Colv=TRUE, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=row.names(smallResult), labRow=colnames(smallResult), 
+          offsetRow=c(1,1), margins=c(8,7))
+
+# Do single genes
+markers = as.matrix(data[c('CD44', 'FUT4', 'PROM1'),])
+heatmap.2(markers, cexRow=1.2, main="Enrichment of FACS marker signatures \n in Molecular Subtype", scale="none",
+          Rowv=NULL, Colv=TRUE, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=colnames(markers), labRow=c('CD44', 'CD15', 'CD133'), 
           offsetRow=c(1,1), margins=c(8,7))
