80ff92920c4b6da172aca12eb7ba0c94ddaa314a
dvbrown <dvbrown@student.unimelb.edu.au>
2014-09-09 16:56:48 +1000

    added the coxph

--- FACSmarkerTCGA/140703_coxphRNAseq.R
+++ FACSmarkerTCGA/140703_coxphRNAseq.R
@@ -96,6 +96,7 @@ clin = clinical[matched, c("CDE_DxAge", "CDE_survival_time", "CDE_vital_status",
                            "G_CIMP_STATUS","GeneExp_Subtype", "X_EVENT","days_to_tumor_progression", "gender")]
 verhaakSubtype = bindGeneExprClinical(clin, bigResult, signatures)
 verhaakSubtype = sort.dataframe(verhaakSubtype, 'colours')
+# write.table(verhaakSubtype, '~/Documents/public-datasets/cancerBrowser/deDupAgilent/results/140909_agilentSubtypeCall.txt', sep='\t')
 # Subset clinical data for intersect
 # Call the FACS subtype
 verhaakSubtypeCall = callMarkerSubtype(verhaakSubtype, 0, 0)
@@ -108,3 +109,28 @@ boundData = merge.data.frame(clin, verhaakSubtypeCall, by.x="row.names", by.y="r
 boundData = sort.dataframe(boundData, "subtype")
 row.names(boundData) = boundData$Row.names
 boundData$subtype = as.factor(boundData$subtype)
+
+############################################# Analysing the data for survival ##################################
+data.surv = Surv(boundData$CDE_survival_time, event=boundData$X_EVENT)
+xtabs(~ CDE_DxAge + subtype, data=boundData)
+
+coxObj = coxph(data.surv ~ CDE_DxAge + subtype, data=boundData, na.action="na.omit")
+summary(coxObj)
+
+# Test proportional hazars assumption. testing for a non-zero slope in a generalized linear regression of the scaled Schoenfeld residuals on functions of time
+time.coxProp <- cox.zph(coxObj, transform = 'log')
+plot(time.coxProp)
+abline(h=0, lty=3, col='red')
+
+# Make a dummy dataframe to allow Coxph lot. Take the mean age of the 2 groups
+dummy <- data.frame(subtype=c('CD44','CD133'), 
+                    CDE_DxAge=c(mean(boundData$CDE_DxAge[boundData$subtype %in% 'CD44'], na.rm=T), 
+                                mean(boundData$CDE_DxAge[boundData$subtype %in% 'CD133'], na.rm=T)))
+
+plot(survfit(coxObj, newdata=dummy), xlab="Survival Time (days)", ylab="Survival Probability", col=c("red",'blue'), 
+     main='FACS marker coexpression signature in \nGlioblastoma multiforme adjusted for age')
+legend('topright', c('CD133', 'CD44'), col=c("red",'blue'), lwd=2, cex=1.2, bty='n', xjust=0.5, yjust=0.5)
+# text(locator(1),labels='p = 0.053', cex=1) #add the p-value to the graph
+
+coxObjTmz = coxph(data.surv ~ CDE_DxAge + CDE_chemo_adjuvant_tmz + subtype , data=boundData, na.action="na.omit")
+summary(coxObjTmz)
