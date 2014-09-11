e0174d5a0d9bb069f10845df82ef09eb4771d3a4
dvbrown <dvbrown@student.unimelb.edu.au>
2014-09-11 16:15:55 +1000

    process shats

---
+++ publicGPSC/140911_shats2011.R
@@ -0,0 +1,72 @@
+library(limma)
+library(affy)
+library(gplots)
+library(ggplot2)
+source('~/Documents/Rscripts/120704-sortDataFrame.R')
+source('~/Documents/Rscripts/multiplot.R')
+
+##################### Build the genelist ###########################
+
+library(annotate)
+library(hgu133plus2.db)
+geneIDs = ls(hgu133plus2ENTREZID)
+
+#get gene ID numbers from the annptation package allowing for multiple probes to match mulitple genes
+geneSymbols <- as.character(unlist(lapply(mget(geneIDs,env=hgu133plus2SYMBOL),
+                                          function (symbol) { return(paste(symbol,collapse="; ")) } )))
+geneNames <- as.character(unlist(lapply(mget(geneIDs,env=hgu133plus2GENENAME),
+                                        function (name) { return(paste(name,collapse="; ")) } )))
+unigene <- as.character(unlist(lapply(mget(geneIDs,env=hgu133plus2UNIGENE),
+                                      function (unigeneID) { return(paste(unigeneID,collapse="; ")) } )))
+
+#strip the Hs from the start of unigene reference
+unigene <- gsub("Hs\\.","",unigene)
+
+#read the gene annotations into a dataframe for use in the topTable function of limma
+genelist <- data.frame(GeneID=geneIDs,GeneSymbol=geneSymbols,GeneName=geneNames)
+rm(geneSymbols, geneNames, unigene)
+
+##################### IO ###########################
+setwd('~/Documents/public-datasets/GPSC_subgroups/shats2010/GSE24716_RAW/')
+list.files()
+
+rawData = ReadAffy()
+dm = readTargets('../designMatrix.txt')
+myPalette <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
+
+par(mfrow=(c(2,1)))
+# boxData = exprs(rawData)
+#colnames(boxData) = dm$cellLine
+boxplot(rawData, col=rainbow(21), main='Shats unNormalised data', las=1, mar=c(5,5,5,2))
+data = rma(rawData, verbose=T)
+norm = exprs(data)
+colnames(norm) = dm$cellLine
+boxplot(norm, col=rainbow(21), main='Shats normalised data', las=2, mar=c(5,5,5,2))
+par(mfrow=(c(1,1)))
+##################### Render the prinical components ###########################
+
+dm$patient = as.factor(dm$patient)
+pca = princomp(norm)
+pcaDf = as.data.frame(cbind(pca$loadings[,1], pca$loadings[,2]))
+pcaDf = cbind(pcaDf, dm)
+dd_text = dm$subpopulation
+
+g = ggplot(data=pcaDf, aes(x=V1, y=V2, color=subpopulation)) + 
+    geom_point(shape=19) + #geom_smooth(method=lm, colour='red') +
+    xlab("PC1") + ylab("PC2") + # Set axis labels
+    ggtitle("Principal component analysis Shats 2011") +  # Set title
+    theme_bw(base_size=18)
+g
+g2 = ggplot(data=pcaDf, aes(x=V1, y=V2, color=patient)) + 
+    geom_point(shape=19) + #geom_smooth(method=lm, colour='red') +
+    xlab("PC1") + ylab("PC2") + # Set axis labels
+    ggtitle("Principal component analysis Shats 2011") +  # Set title
+    theme_bw(base_size=18)
+g2
+g3 = ggplot(data=pcaDf, aes(x=V1, y=V2, color=growth)) + 
+    geom_point(shape=19) + #geom_smooth(method=lm, colour='red') +
+    xlab("PC1") + ylab("PC2") + # Set axis labels
+    ggtitle("Principal component analysis Shats 2011") +  # Set title
+    theme_bw(base_size=18)
+g3
+multiplot(g, g2,g3, cols=2)
