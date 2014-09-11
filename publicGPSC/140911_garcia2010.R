6c86e9df865f52653c1829eadbecba81258e55f5
dvbrown <dvbrown@student.unimelb.edu.au>
2014-09-11 15:22:54 +1000

    finished signatures

--- publicGPSC/140911_garcia2010.R
+++ publicGPSC/140911_garcia2010.R
@@ -5,6 +5,27 @@ library(ggplot2)
 source('~/Documents/Rscripts/120704-sortDataFrame.R')
 source('~/Documents/Rscripts/multiplot.R')
 
+##################### Build the genelist ###########################
+
+library(annotate)
+library(hgu133plus2.db)
+geneIDs = ls(hgu133plus2ENTREZID)
+
+#get gene ID numbers from the annptation package allowing for multiple probes to match mulitple genes
+geneSymbols <- as.character(unlist(lapply(mget(geneIDs,env=hgu133plus2SYMBOL),
+                                          function (symbol) { return(paste(symbol,collapse="; ")) } )))
+geneNames <- as.character(unlist(lapply(mget(geneIDs,env=hgu133plus2GENENAME),
+                                        function (name) { return(paste(name,collapse="; ")) } )))
+unigene <- as.character(unlist(lapply(mget(geneIDs,env=hgu133plus2UNIGENE),
+                                      function (unigeneID) { return(paste(unigeneID,collapse="; ")) } )))
+
+#strip the Hs from the start of unigene reference
+unigene <- gsub("Hs\\.","",unigene)
+
+#read the gene annotations into a dataframe for use in the topTable function of limma
+genelist <- data.frame(GeneID=geneIDs,GeneSymbol=geneSymbols,GeneName=geneNames)
+rm(geneSymbols, geneNames, unigene)
+
 ##################### IO ###########################
 
 setwd('~/Documents/public-datasets/GPSC_subgroups/garcia2010/GSE18015_RAW/')
@@ -17,11 +38,11 @@ myPalette <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
 par(mfrow=(c(2,1)))
 # boxData = exprs(rawData)
 # colnames(boxData) = dm$cellLine
-boxplot(rawData, col=rainbow(21), main='Bhat unNormalised data', las=1, mar=c(5,5,5,2))
+#boxplot(rawData, col=rainbow(21), main='Bhat unNormalised data', las=1, mar=c(5,5,5,2))
 data = rma(rawData, verbose=T)
 norm = exprs(data)
 colnames(norm) = dm$cellLine
-boxplot(norm, col=rainbow(21), main='Bhat normalised data', las=2, mar=c(5,5,5,2))
+#boxplot(norm, col=rainbow(21), main='Bhat normalised data', las=2, mar=c(5,5,5,2))
 par(mfrow=(c(1,1)))
 
 ##################### Render the prinical components ###########################
@@ -74,6 +95,12 @@ heatmap.2(topNorm, cexRow=0.8, main="Gene expression profiles CD133 sorted GPSCs
           ColSideColors=as.character(dm$colour), labRow=NA, xlab="Aglent samples", labCol=name, 
           offsetRow=c(1,1), margins=c(10,4))
 
+# Tkae the probe averages
+summariseData = avereps(data, ID=genelist$GeneSymbol)
+# Remove NAs
+summariseData = summariseData[row.names(summariseData) != 'NA' ,]
+
 ##################### Export data ###########################
 write.table(norm, '../analysis/140826_rmaNormalised.txt', sep='\t')
+write.table(summariseData, '../analysis/140911_garciaGEM.txt', sep='\t')
 write.table(topNorm, '../analysis/140826_top500.txt', sep='\t')
--- publicGPSC/140911_garcia_signatures.R
+++ publicGPSC/140911_garcia_signatures.R
@@ -4,7 +4,7 @@ library(gplots)
 ##################### IO ###########################
 setwd('~/Documents/public-datasets/GPSC_subgroups/garcia2010/analysis/')
 
-data = read.delim('140826_rmaNormalised.txt', row.names=1)
+data = read.delim('140911_garciaGEM.txt', row.names=1)
 dataM = as.matrix(data)
 dm = read.delim('../designMatrix.txt')
 # Just the untreated controls
@@ -21,6 +21,7 @@ bigSigs = list("CD133" = row.names(cd133Sig), "CD44" = row.names(cd44Sig), "CD15
 rm(cd133Sig, cd44Sig, cd15, aldh1, itag6, l1cam)
 
 ##################### GSVA ###########################
+# dataM[is.na(dataM)] <- 0
 bigResult = gsva(dataM, bigSigs,  rnaseq=F, verbose=T, parallel.sz=1)
 bigResult = t(bigResult$es.obs)
 
@@ -28,5 +29,28 @@ dm$colour = "black"
 dm$colour[dm$subpopulation %in% 'CD133+'] = 'blue'
 dm$colour[dm$subpopulation %in% 'CD133-'] = 'red'
 myPalette <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
-
+name = paste(dm$patient, dm$subpopulation)
 ##################### heatMaps ###########################
+
+heatmap.2(t(bigResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs", scale="none",
+          Rowv=NULL, Colv=TRUE, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=name, labRow=colnames(bigResult), 
+          offsetRow=c(1,1), margins=c(8,7))
+
+heatmap.2(t(bigResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs", scale="none",
+          Rowv=T, Colv=as.character(dm$colour), keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=name, labRow=colnames(bigResult), 
+          offsetRow=c(1,1), margins=c(8,7))
+
+smallResult = bigResult[,c(1:3)]
+heatmap.2(t(smallResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs", scale="none",
+          Rowv=T, Colv=as.character(dm$colour), keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=name, labRow=colnames(smallResult), 
+          offsetRow=c(1,1), margins=c(8,7))
+
+# Do single genes
+markers = as.matrix(data[c('CD44', 'FUT4', 'PROM1'),])
+heatmap.2(markers, cexRow=1.2, main="Enrichment of FACS marker mRNAs \n in FACS sorted GPSCs", scale="row",
+          Rowv=T, Colv=as.character(dm$colour), keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
+          ColSideColors=as.character(dm$colour), xlab="Bhat et al 2013 samples", labCol=name, labRow=c('CD44', 'CD15', 'CD133'), 
+          offsetRow=c(1,1), margins=c(8,7))
