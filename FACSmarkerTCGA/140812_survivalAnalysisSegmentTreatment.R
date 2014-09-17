575ba422f91088fb5a02b9596f8d2765aac8053a
dvbrown <dvbrown@student.unimelb.edu.au>
2014-09-17 12:52:31 +1000

    lunchtime commit

--- FACSmarkerTCGA/140812_survivalAnalysisSegmentTreatment.R
+++ FACSmarkerTCGA/140812_survivalAnalysisSegmentTreatment.R
@@ -18,22 +18,6 @@ matched = intersect(row.names(clinical), row.names(verhaakSubtypeCall))
 # Subset clinical data for intersect
 clin = clinical[matched, c("CDE_DxAge", "CDE_survival_time", "CDE_vital_status","X_EVENT", "gender", "days_to_last_followup", "CDE_chemo_adjuvant_tmz", "CDE_radiation_adjuvant")]
 
-# If the survival is NA, use the value for days to last followup
-# clin$survival = clin$CDE_survival_time
-# clin$survival[is.na(clin$survival)] = clin$days_to_last_followup[is.na(clin$survival)]
-
-############################################## Segment the subtypes into CD133 CD44 #############################################
-
-# Make an object to plot density gram
-lattPlot = data.frame(as.numeric(c(verhaakSignature[,"CD133"], verhaakSignature[,"CD44"])), 
-                      c(rep("CD133", nrow(verhaakSignature)), rep("CD44", nrow(verhaakSignature))))
-colnames(lattPlot) = c('value', "signature")
-
-ggplot(lattPlot, aes(value, fill = signature)) + geom_density(alpha = 0.2) +
-    xlab("Signature score") + ylab("Density") + # Set axis labels
-    ggtitle("Distribution of CD133 and \nCD44 signature scores") +  # Set title
-    coord_cartesian(xlim = c(-1, 1)) + theme_bw(base_size=20) + geom_vline(xintercept=0, colour="red")
-
 ############################################## bind the clinical and subtyping info together #############################################
 
 boundData = merge.data.frame(clin, verhaakSubtypeCall, by.x="row.names", by.y="row.names")
@@ -42,31 +26,58 @@ row.names(boundData) = boundData$Row.names
 boundData$subtype = as.factor(boundData$subtype)
 boundData$gender = as.factor(boundData$gender)
 
-############################################# Subset the CD133 cases and then group by TMZ exposure ##################################
+# Fix up the GCIMP to only have true and false
+boundData$G_CIMP_STATUS = as.character(boundData$G_CIMP_STATUS)
+boundData$G_CIMP_STATUS[boundData$G_CIMP_STATUS %in% 'G-CIMP'] = TRUE
+boundData$G_CIMP_STATUS[!boundData$G_CIMP_STATUS %in% 'TRUE'] = FALSE
+boundData$G_CIMP_STATUS = as.factor(boundData$G_CIMP_STATUS)
+
+############################################# Subset the Subtype cases indivdually ##################################
 
 cd133Patients = boundData[boundData$subtype %in% "CD133",]
+cd44Patients = boundData[boundData$subtype %in% "CD44",]
 
 ############################################# Analysing the data for survival ##################################
 
 #generate the survival object and plot a Kaplan-Meier
-data.surv = Surv(cd133Patients$CDE_survival_time, event=cd133Patients$X_EVENT)
+data.surv.cd133 = Surv(cd133Patients$CDE_survival_time, event=cd133Patients$X_EVENT)
+data.surv.cd44 = Surv(cd44Patients$CDE_survival_time, event=cd44Patients$X_EVENT)
 
-sur.fit = survfit(data.surv~CDE_chemo_adjuvant_tmz, cd133Patients)
+sur.fit.cd133 = survfit(data.surv.cd133~CDE_chemo_adjuvant_tmz, cd133Patients)
+sur.fit.cd44 = survfit(data.surv.cd44~CDE_chemo_adjuvant_tmz, cd44Patients)
 
-plot(sur.fit, main='TCGA GBM cohort CD133 patients classified by treatment',ylab='Survival probability',xlab='survival (days)', 
+par(mfrow=c(2,1))
+plot(sur.fit.cd133, main='TCGA GBM cohort CD133 patients classified by treatment',ylab='Survival probability',xlab='survival (days)', 
      col=c("red",'blue'),
-     xlim=c(0,1600), 
      cex=1.75, conf.int=F, lwd=1.33)
-
 legend('topright', c('FALSE', 'TRUE'), title="Adjuvant temozolomide",
        col=c("red",'blue'),
        lwd=1.33, cex=1.2, bty='n', xjust=0.5, yjust=0.5)
 
-summary(data.surv)
+summary(data.surv.cd133)
+test.cd133 = surv_test(data.surv.cd133~as.factor(cd133Patients$CDE_chemo_adjuvant_tmz))
+test.cd133
+# text(locator(1),labels='p=0.137', cex=1) #add the p-value to the graph
+
+plot(sur.fit.cd44, main='TCGA GBM cohort CD44 patients classified by treatment',ylab='Survival probability',xlab='survival (days)', 
+     col=c("red",'blue'),
+     cex=1.75, conf.int=F, lwd=1.33)
+legend('topright', c('FALSE', 'TRUE'), title="Adjuvant temozolomide",
+       col=c("red",'blue'),
+       lwd=1.33, cex=1.2, bty='n', xjust=0.5, yjust=0.5)
+summary(data.surv.cd44)
 #test for a difference between curves
-test = surv_test(data.surv~as.factor(cd133Patients$CDE_chemo_adjuvant_tmz))#, subset=!boundData$subtype %in% "intermediate")
-test
-text(locator(1),labels='p=0.137', cex=1) #add the p-value to the graph
+test.cd44 = surv_test(data.surv.cd44~as.factor(cd44Patients$CDE_chemo_adjuvant_tmz))
+test.cd44
+# text(locator(1),labels='p=0.0165', cex=1) #add the p-value to the graph
+
+
+
+
+
+
+
+
 
 #### Look at radiation ####
 sur.fit = survfit(data.surv~CDE_radiation_adjuvant, cd133Patients)
@@ -86,29 +97,7 @@ test = surv_test(data.surv~as.factor(cd133Patients$CDE_radiation_adjuvant))#, su
 test
 text(locator(1),labels='p=1.4e-06', cex=1) #add the p-value to the graph
 
-############################################# Do the same for CD44 ##################################
 
-cd44Patients = boundData[boundData$subtype %in% "CD44",]
-
-#generate the survival object and plot a Kaplan-Meier
-data.surv = Surv(cd44Patients$CDE_survival_time, event=cd44Patients$X_EVENT)
-
-sur.fit = survfit(data.surv~CDE_chemo_adjuvant_tmz, cd44Patients)
-
-plot(sur.fit, main='TCGA GBM cohort CD44 patients classified by treatment',ylab='Survival probability',xlab='survival (days)', 
-     col=c("red",'blue'),
-     xlim=c(0,1600), 
-     cex=1.75, conf.int=F, lwd=1.33)
-
-legend('topright', c('FALSE', 'TRUE'), title="Adjuvant temozolomide",
-       col=c("red",'blue'),
-       lwd=1.33, cex=1.2, bty='n', xjust=0.5, yjust=0.5)
-
-summary(data.surv)
-#test for a difference between curves
-test = surv_test(data.surv~as.factor(cd44Patients$CDE_chemo_adjuvant_tmz))#, subset=!boundData$subtype %in% "intermediate")
-test
-text(locator(1),labels='p=0.0165', cex=1) #add the p-value to the graph
 
 #### CD44 and radiation ####
 data.surv = Surv(cd44Patients$CDE_survival_time, event=cd44Patients$X_EVENT)
