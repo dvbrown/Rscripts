6c30d9ff810ac39c043d8f099721bd07ec3988ae
dvbrown <dvbrown@student.unimelb.edu.au>
2014-09-24 16:55:41 +1000

    making heat maps

--- publicGPSC/140924_zarkoob_signatures.R
+++ publicGPSC/140924_zarkoob_signatures.R
@@ -29,3 +29,34 @@ dm$colour = c("blue", "red", "blue", "red", "blue", "red", "blue", "red")
 dm$colour = as.factor(dm$colour)
 myPalette <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
 name = paste(dm$source, dm$tissue, dm$subpopulation)
+
+bigResult = bigResult[c(1:4),]
+##################### heatMaps ###########################
+
+heatmap.2(t(bigResult), cexRow=1.5, cexCol=1.5, main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs", scale="none",
+          Rowv=NULL, Colv=TRUE, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", 
+          ColSideColors=as.character(dm$colour[1:4]), labCol=name, labRow=colnames(bigResult), 
+          offsetRow=c(1,1), margins=c(14,7))
+
+# heatmap.2(t(bigResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs", scale="none",
+#           Rowv=T, Colv=as.character(dm$colour), keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
+#           ColSideColors=as.character(dm$colour), labCol=name, labRow=colnames(bigResult), 
+#           offsetRow=c(1,1), margins=c(14,7))
+
+smallResult = bigResult[,c(1:3)]
+# heatmap.2(t(smallResult), cexRow=1.2, main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs", scale="none",
+#           Rowv=T, Colv=as.character(dm$colour), keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
+#           ColSideColors=as.character(dm$colour), labCol=name, labRow=colnames(smallResult), 
+#           offsetRow=c(1,1), margins=c(14,7))
+
+heatmap.2(t(smallResult), cexRow=1.5, cexCol=1.5,scale="none", #main="Enrichment of FACS marker signatures \n in FACS sorted GPSCs",
+          Rowv=NULL, Colv=T, keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="column", 
+          ColSideColors=as.character(dm$colour), labCol=name, labRow=colnames(smallResult), 
+          offsetRow=c(1,1), margins=c(14,7))
+
+# Do single genes
+markers = as.matrix(data[c('CD44', 'FUT4', 'PROM1'),])
+heatmap.2(markers, cexRow=1.2, main="Enrichment of FACS marker mRNAs \n in FACS sorted GPSCs", scale="row",
+          Rowv=T, Colv=as.character(dm$colour), keysize=1, trace="none", col=myPalette, density.info="none", dendrogram="row", 
+          ColSideColors=as.character(dm$colour),  labCol=name, labRow=c('CD44', 'CD15', 'CD133'), 
+          offsetRow=c(1,1), margins=c(14,7))
