804269fee8e52a13c5a6db3237ff99524181b4d6
dvbrown <dvbrown@student.unimelb.edu.au>
2014-10-16 13:12:45 +1100

    wrote the subtype calls of both RNAseq and Agilent

--- FACSmarkerTCGA/methylation/141009_tcgaMethylCanBrowser.R
+++ FACSmarkerTCGA/methylation/141009_tcgaMethylCanBrowser.R
@@ -2,5 +2,15 @@
 library(sqldf)
 library(gplots)
 library(RColorBrewer)
-setwd("~/Documents/public-datasets/TCGA/methylation/")
+setwd("~/Documents/public-datasets/cancerBrowser/methylation/")
 list.files()
+
+# Open a connection to database
+db <- dbConnect(SQLite(), dbname="~/Documents/public-datasets/cancerBrowser/tcgaData.sqlite")
+dbListTables(db)
+clinicalRNAseq = dbReadTable(db, "subtypeCallRNAseq")
+clinicalAgilent = dbReadTable(db, "subtypeCallAgilent")
+
+# Start with h27k
+h27 = read.delim("TCGA_GBM_hMethyl27-2014-08-22/genomicMatrix", row.names =1)
+head(h27)
