11c8ec09854322f86a14dbc557dcde2d97d4fd18
dvbrown <dvbrown@student.unimelb.edu.au>
2014-10-21 13:26:49 +1100

    switch script to generate the subtypes Agilent

--- FACSmarkerTCGA/141021_cd133Gcimp.R
+++ FACSmarkerTCGA/141021_cd133Gcimp.R
@@ -1,14 +1,13 @@
 # Examine the relationship between G-CIMP and CD133 subtype as well as absolute expression
 library(sqldf)
 library(ggplot2)
-
+source("~/Documents/Rscripts/multiplot.R")
 setwd("~/Documents/public-datasets/cancerBrowser/cd133_gCIMP/")
 
 ############################ IO ################################
 db = dbConnect(SQLite(), dbname="~/Documents/public-datasets/cancerBrowser/tcgaData.sqlite")
 dbListTables(db)
 clinical = dbReadTable(db, "clinicalAllPatients", row.names=1)
-agilentGem = dbReadTable(db, "AgilentGem", row.names=1)
 
 rnaSeqGem = dbReadTable(db, "RNAseqGem", row.names=1)
 markerScore = dbReadTable(db, "markerScoresRNAseq", row.names=1)
@@ -18,19 +17,28 @@ markerScore = dbReadTable(db, "markerScoresRNAseq", row.names=1)
 markerCIMP = merge.data.frame(clinical, markerScore, by.x=0, by.y=0)
 markerCIMP = markerCIMP[!markerCIMP$G_CIMP_STATUS %in% "",]
 
-ggplot(markerCIMP, aes(x=G_CIMP_STATUS, y=CD133, fill=G_CIMP_STATUS)) + geom_boxplot() +
-    scale_colour_manual(values=c("aqua", "orange")) +
-    xlab("CD133 signatures") + ylab("CD44 signatures") + # Set axis labels
-    ggtitle("No difference in CD133 coexpression signature by CIMP status") +  # Set title
-    guides(fill=FALSE) + geom_jitter() +  theme_bw(base_size=18)
+sig = ggplot(markerCIMP, aes(x=G_CIMP_STATUS, y=CD133, fill=G_CIMP_STATUS)) + geom_boxplot() +
+        scale_colour_manual(values=c("aqua", "orange")) +
+        xlab("") + ylab("enrichment score") + # Set axis labels
+        ggtitle("No difference in CD133 coexpression\n signature by CIMP status") +  # Set title
+        guides(fill=FALSE) + geom_jitter() +  theme_bw(base_size=18)
 
 ############################ Extract the raw expression values ###############################
 markermRNA = rnaSeqGem[,c('PROM1', 'FUT4', 'CD44')]
 gemCIMP = merge.data.frame(clinical, markermRNA, by.x=0, by.y=0)
 gemCIMP = gemCIMP[!gemCIMP$G_CIMP_STATUS %in% "",]
 
-ggplot(gemCIMP, aes(x=G_CIMP_STATUS, y=PROM1, fill=G_CIMP_STATUS)) + geom_boxplot() +
-    scale_colour_manual(values=c("aqua", "orange")) +
-    xlab("CD133 signatures") + ylab("CD44 signatures") + # Set axis labels
-    ggtitle("Minor difference in CD133 coexpression signature by CIMP status") +  # Set title
-    guides(fill=FALSE) + geom_jitter() +  theme_bw(base_size=18)
+mRNA = ggplot(gemCIMP, aes(x=G_CIMP_STATUS, y=PROM1, fill=G_CIMP_STATUS)) + geom_boxplot() +
+        scale_colour_manual(values=c("aqua", "orange")) +
+        xlab("") + ylab("mRNA expression") + # Set axis labels
+        ggtitle("Minor difference in CD133 mRNA expression\n by CIMP status") +  # Set title
+        guides(fill=FALSE) + geom_jitter() +  theme_bw(base_size=18)
+
+multiplot(sig, mRNA, cols=2)
+
+
+
+############################ Repeat with Agilent data ###############################
+library(GSVA)
+agilentGem = dbReadTable(db, "AgilentGem", row.names=1)
+
