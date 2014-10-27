d5899631c1357d1436307b272d6451f41efd6358
dvbrown <dvbrown@student.unimelb.edu.au>
2014-10-27 14:21:15 +1100

    almost at the end of the script

--- 141023_cellBiolAssaySummary/141023_cellBiolAssayInvasion.R
+++ 141023_cellBiolAssaySummary/141023_cellBiolAssayInvasion.R
@@ -34,6 +34,7 @@ list.files()
 # Intialise and write into database
 db <- dbConnect(SQLite(), dbname="assaySummary.sqlite")
 invasion = read.delim("141028_invasionSummary.txt")
+invasion = invasion[c(1:26,28:31),]
 # Divide by 1000 to make axis nicer
 invasion$mean = rowMeans(invasion[,c(4:6)], na.rm=T) / 1000
 invasion$sd = apply(invasion[,c(4:6)], 1, sd, na.rm=T) / 1000
@@ -54,12 +55,11 @@ spherePlot = ggplot(invasion[invasion$treatment %in% 'FALSE',],
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
 
 # Normalise by double negative. As #035 has no DN must discard it
-dnNormSphere = combineExperiments(invasion, calcInvNormalised)[c(1:9),]
+dnNormSphere = combineExperiments(invasion, calcInvNormalised)
 # Need to mung #041 myself
-fourty = read.delim("141023_invasion041.txt")[c(1,3,5,8),]
-dnNormSphere = rbind(dnNormSphere, fourty)
+fourty = read.delim("141023_invasion041.txt")
+dnNormSphere = rbind(dnNormSphere, fourty[fourty$treatment %in% 'FALSE',])
 
-tail(invasion)
 spherePlotNorm = ggplot(dnNormSphere, aes(x=patient, y=normDN, fill=subpop)) + 
     scale_fill_manual(values=color) +
     #scale_fill_manual(values=bw) +
@@ -67,3 +67,33 @@ spherePlotNorm = ggplot(dnNormSphere, aes(x=patient, y=normDN, fill=subpop)) +
     xlab("PDGC") + ylab("Surface area relative to \nCD44-/CD133-") +
     ggtitle("Gliomasphere surface area at day 7 by \nmarker status") +  # Set title
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
+
+################## matrix ############################
+normaliseNoMat= function(dataFrame) {
+    matrixFalse = dataFrame[dataFrame[,"treatment"] %in% "FALSE",]
+    matrixTrue = dataFrame[dataFrame[,"treatment"] %in% "TRUE",]
+    matrixTrue$noMatNorm = matrixTrue$mean / matrixFalse$mean
+    return (matrixTrue)
+}
+
+summariseByFactor = function (dataFrame, factor1, factor2) {
+    require(plyr)
+    # Takes a dataframe with factor information and computes summary statistics based on levels of as least 2 factors
+    # factor 1 and 2 are characters
+    result <- ddply(dataFrame, c(factor1, factor2), summarise,
+                    N = length(dnNorm), mean = mean(dnNorm), sd   = sd(dnNorm), se   = sd / sqrt(N) )
+    return (result)
+
+matNorm = normaliseNoMat(invasion)
+# write.table(matNorm, './invasion/141028_mungInvasion.txt', sep='\t')
+matNorm = read.delim("./invasion/141028_mungInvasion.txt")
+
+matrixNorm= ggplot(matNorm, aes(x=patient, y=dnNorm, fill=subpop)) + 
+    scale_fill_manual(values=color) +
+    #scale_fill_manual(values=bw) +
+    geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+    xlab("PDGC") + ylab("Invasion relative to \nCD44-/CD133-") +
+    ggtitle("Gliomasphere invasion at day 7 by \nmarker status") +  # Set title
+    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
+
+summariseByFactor(matNorm, 'subpop', 'treatment')
