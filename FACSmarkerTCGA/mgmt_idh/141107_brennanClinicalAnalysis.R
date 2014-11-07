779c6b60914a7c85bfaaf779db82870965e9255b
dvbrown <dvbrown@student.unimelb.edu.au>
2014-11-07 14:47:48 +1100

    look around committing

--- FACSmarkerTCGA/mgmt_idh/141107_brennanClinicalAnalysis.R
+++ FACSmarkerTCGA/mgmt_idh/141107_brennanClinicalAnalysis.R
@@ -18,7 +18,22 @@ clinical$G_CIMP_status = as.factor(clinical$G_CIMP_status)
 clinical$IDH1_status = as.factor(clinical$IDH1_status)
 clinical$MGMT_Status = as.factor(clinical$MGMT_Status)
 
-xtabs(~ subtype + Molecular_subtype + G_CIMP_status + IDH1_status + MGMT_Status, data=clinical)
+molSubtype = xtabs(~ Molecular_subtype + subtype, data=clinical)
+cimp = xtabs (~ G_CIMP_status + subtype, data=clinical)
+idh = xtabs(~ IDH1_status + subtype, data=clinical)
+mgmt = xtabs(~ MGMT_Status + subtype, data= clinical)
 
+contTable = rbind(molSubtype, cimp, idh, mgmt)
+contTable = as.data.frame(contTable[c(1,3:11),])
+
+# Convert to percentage
+contTable$cd133Percent = (contTable$CD133 / (contTable$CD133 + contTable$CD44)) * 100
+contTable$cd44Percent = (contTable$CD44 / (contTable$CD133 + contTable$CD44)) * 100
+write.table(contTable, "./141107_mungThis.txt", sep='\t')
+
+plotData 
+
+ggplot(data = contTable, aes(x = Type.of.Behavior, y = Sample.Size, fill = Stage.of.Change)) + 
+    geom_bar()
 
 dbDisconnect(db)
