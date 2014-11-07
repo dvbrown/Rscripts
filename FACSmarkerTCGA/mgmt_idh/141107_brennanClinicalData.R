2cd476348a00049fb8176bb06e30a1491d251176
dvbrown <dvbrown@student.unimelb.edu.au>
2014-11-07 13:59:24 +1100

    munged brennan data successfully

--- FACSmarkerTCGA/mgmt_idh/141107_brennanClinicalData.R
+++ FACSmarkerTCGA/mgmt_idh/141107_brennanClinicalData.R
@@ -12,8 +12,8 @@ dbListTables(db)
 setwd("~/Documents/public-datasets/TCGA/2013_dataFreeze/")
 brennan = read.delim("Cell2013Brennan.txt", row.names=1, na.strings="")
 # dbWriteTable(conn = db, name = "brennanClinical", value = brennan, row.names = TRUE)
-
 subtypeAllPatients = dbReadTable(db, "clinicalAllPatients", row.names=T)
+
 subtypeAllPatients$rowName = substr(subtypeAllPatients$Row_names__1, 1, 12)
 subtypeName = gsub("\\.", "-", subtypeAllPatients$Row_names__1)
 subtypeName1 = substr(subtypeName, 1, 12)
@@ -22,6 +22,16 @@ subtypeName1 = substr(subtypeName, 1, 12)
 subtypeName3 = subtypeName1[!duplicated(subtypeName1)]
 subtypeAllPatients  = subtypeAllPatients [!duplicated(subtypeName1),]
 row.names(subtypeAllPatients) = subtypeName3
+# Get rid of 1 recurent
+subtypeAllPatients = subtypeAllPatients[!subtypeAllPatients$Row_names__1 %in% 'TCGA.06.0171.02',]
 
  # Merge the data together
 bigDF = merge.data.frame(subtypeAllPatients, brennan, by.x=0, by.y=0)
+bigDF1 = bigDF[,c("Row_names__1", "CDE_chemo_adjuvant_tmz", "CDE_radiation_any", "subtype", "Secondary_or_recurrent", "Age_at_procedure",
+                 "G.CIMP_status", "IDH1_status", "Molecular_subtype", "Therapy_class", "Vital_status", "OS_.days.", "Progression_status", "PFS_.days.")]
+
+# IDH status to mutant
+bigDF1$IDH1_status = as.character(bigDF1$IDH1_status)
+bigDF1$IDH1_status[bigDF1$IDH1_status %in% c("R132C", "R132G", "R132H")] = 'MT'
+bigDF1$IDH1_status = as.factor(bigDF1$IDH1_status)
+levels(bigDF1$IDH1_status) = c("WT", "MT")#, "R132C", "R132G", "R132H")
