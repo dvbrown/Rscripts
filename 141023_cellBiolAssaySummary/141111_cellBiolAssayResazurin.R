53caa4881fa427dc42766bb7eb38b3e151a662b2
dvbrown <dvbrown@student.unimelb.edu.au>
2014-11-12 15:25:51 +1100

    finish off script now invasion

--- 141023_cellBiolAssaySummary/141111_cellBiolAssayResazurin.R
+++ 141023_cellBiolAssaySummary/141111_cellBiolAssayResazurin.R
@@ -5,7 +5,7 @@ library(plyr)
 source("~/Documents/Rscripts/cellBiologyAnalysisFunctions.R")
 source('~/Documents/Rscripts/140211_multiplotGgplot2.R')
 
-setwd("~/Documents/Cell_biology/141023_summary/")
+setwd("~/Documents/Cell_biology/141023_summary/resazurin/")
 list.files()
 
 # Intialise and write into database
@@ -22,22 +22,22 @@ growth$cv = growth$sd / growth$mean * 100
 write.table(growth, "141112_growthSummary.txt", sep='\t')
 # I munged the data in excel and made the appropriate normalisations.
 # The important columns are growth std and tmzstd
-growthMung = read.delim("141112_growthNormalise.txt")
+growthMung = read.delim("141112_growthNormaliseConfident.txt")
 
 bw = c("grey21", "grey82", "grey52", "grey97")
 color = c("chartreuse4", "skyblue2", "gold", "orangered1")
 
 ################## Growth assay ############################
 # Plot the raw results
-growthPlot = ggplot(growth[growth$treatment %in% 'DMSO',], 
-                     aes(x=patient, y=mean, fill=subpop)) + 
-    scale_fill_manual(values=color) +
-    #scale_fill_manual(values=bw) +
-    geom_bar(stat="identity", position=position_dodge(), colour="black") + 
-    geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2, position=position_dodge(0.9)) +
-    xlab("PDGC") + ylab("Fluorescent intensity") +
-    ggtitle("Growth at day 7 by \nmarker status") +  # Set title
-    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
+# growthPlot = ggplot(growth[growth$treatment %in% 'DMSO',], 
+#                      aes(x=patient, y=mean, fill=subpop)) + 
+#     scale_fill_manual(values=color) +
+#     #scale_fill_manual(values=bw) +
+#     geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+#     geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2, position=position_dodge(0.9)) +
+#     xlab("PDGC") + ylab("Fluorescent intensity") +
+#     ggtitle("Growth at day 7 by \nmarker status") +  # Set title
+#     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
 
 ################### Summarise by marker status and get sem ################### 
 # extract Growth columns
@@ -57,11 +57,12 @@ growthSumPlot = ggplot(growthSummary, aes(x=subpop, y=mean, fill=subpop)) +
     theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 45, hjust = 1), text = element_text(size=24))
 growthSumPlot
 
-pdf(file="./resazurin/141112_growthDMSObw.pdf", useDingbats=F, height=12, width=18)
-growthSumPlot
-dev.off()
+# pdf(file="./resazurin/141112_growthDMSObw.pdf", useDingbats=F, height=12, width=18)
+# growthSumPlot
+# dev.off()
 
 anova(lm(growthStd ~ subpop + patient, data = growth))
+TukeyHSD.aov(aov(growthStd ~ subpop + patient, data = growth), which="subpop")
 
 ################### TMZ analysis ################### 
 tmz = growthMung[growthMung$treatment %in% 'TMZ',]
@@ -71,20 +72,22 @@ tmzSummary <- ddply(tmz, 'subpop', summarise,
                        sd   = sd(TMZstd), se   = sd / sqrt(N) )
 
 tmzSumPlot = ggplot(tmzSummary, aes(x=subpop, y=mean, fill=subpop)) + 
-    #scale_fill_manual(values=color) +  guides(fill=FALSE) +
-    scale_fill_manual(values=bw) +  guides(fill=FALSE) +
+    scale_fill_manual(values=color) +  guides(fill=FALSE) +
+    #scale_fill_manual(values=bw) +  guides(fill=FALSE) +
     geom_bar(stat="identity", position=position_dodge(), colour="black") + 
     geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.2, position=position_dodge(0.9)) +
     xlab("PDGC") + ylab("Temozolomide resistance") +
     ggtitle("Resistance to TMZ at day 7 \nby marker status") +  # Set title
-    theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
+    theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 45, hjust = 1), text = element_text(size=24))
 tmzSumPlot
 
-pdf(file="./resazurin/141112_growthTMZbw.pdf", useDingbats=F, height=12, width=18)
+pdf(file="./141112_growthTMZ.pdf", useDingbats=F, height=12, width=18)
 tmzSumPlot
 dev.off()
 
 anova(lm(TMZstd ~ subpop + patient, data = tmz))
+TukeyHSD.aov(aov(TMZstd ~ subpop + patient, data = tmz), which="subpop")
+t.test(TMZstd ~ subpop, data = tmz[tmz$subpop %in% c('CD44-/CD133-', 'CD44+/CD133+'),])
 
 #### Write into database ####
 dbWriteTable(conn = db, name = "growthData", value = growthData, row.names = TRUE)
