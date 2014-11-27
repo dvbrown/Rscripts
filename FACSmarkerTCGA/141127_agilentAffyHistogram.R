6f292a8a8391b77cd9e1667ae6aaded51500e768
dvbrown <dvbrown@student.unimelb.edu.au>
2014-11-28 10:26:26 +1100

    trying to get gator on GAPDH

--- FACSmarkerTCGA/141127_agilentAffyHistogram.R
+++ FACSmarkerTCGA/141127_agilentAffyHistogram.R
@@ -11,3 +11,15 @@ patients = intersect(colnames(affy), colnames(agilent))
 # Take patients in common
 agilent = agilent[,patients]
 affy = affy[,patients]
+
+affyGAPDH = as.data.frame(t(affy["GAPDH",]))
+affyGAPDH$gene = "GAPDH"
+affyBact = as.data.frame(t(affy["ACTB",]))
+affyBact$gene = "ACTB"
+colnames(affyGAPDH) = c("expression", "gene")
+colnames(affyBact) = c("expression", "gene")
+affyGene = rbind(affyGAPDH, affyBact)
+
+
+agilentGAPDH = as.data.frame(t(agilent["GAPDH",]))
+agilentGAPDH$origin = "Agilent"
