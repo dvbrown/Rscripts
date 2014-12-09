47480feada14c004d5b5a7c7fd448d326bfd3ab0
dvbrown <dvbrown@student.unimelb.edu.au>
2014-12-09 14:48:59 +1100

    z centred copy number

--- rnaSeqBatch1/141209_copyNumberRNAseq.R
+++ rnaSeqBatch1/141209_copyNumberRNAseq.R
@@ -16,6 +16,12 @@ annotateIds = function(geneList)  {
     return (list)
 }
 
+moveAv <- function(x,n=100){
+    # X is the time series a numeric vector
+    # n is the window size to use. 100 is from Patel et al 2014 single cell GBM
+    return (filter(x,rep(1/n,n), sides=2, circular=F))
+}
+
 setwd("~/Documents/RNAdata/danBatch1/bowtieGem/revHTSeq/copyNumber/")
 # cpm = read.delim("../GLMedgeR/131021_normalisedCPM.txt", row.names=1)
 # head(cpm)
@@ -33,20 +39,31 @@ bedLike = read.delim("141209_bedLike.txt")
 chr = as.factor(c(1:22, "X", "Y"))
 bedLike = bedLike[bedLike$chromosome_name %in% chr,]
 droplevels(bedLike$chromosome_name)
-seqNames = as.character(bedLike$chromosome_name)
 
 # Implement the copy number conversion from Patel et al
 # Sort by chromosome and start position
 bedLike = bedLike[order(bedLike$chromosome, bedLike$start_position),]
+copyNum = bedLike[,]
+copyNum$GIC_011 = as.numeric(moveAv(copyNum$GIC_011))
+copyNum$GIC_020 =  as.numeric(moveAv(copyNum$GIC_020))
+copyNum$GIC_034 =  as.numeric(moveAv(copyNum$GIC_034))
+copyNum$GIC_035 =  as.numeric(moveAv(copyNum$GIC_035))
+copyNum$GIC_039 =  as.numeric(moveAv(copyNum$GIC_039))
+copyNum$GIC_041 =  as.numeric(moveAv(copyNum$GIC_041))
+copyNum$begining = paste(copyNum$chromosome_name, copyNum$start_position, sep="_")
 
-moveAv <- function(x,n=100){
-    return (filter(x,rep(1/n,n), sides=2))
-}
-moveAv(bedLike$GIC_011)
+# Extract and z center the copy number
+copMat = as.matrix(copyNum[,c(7:12)])
+copMat = scale(copMat)
+copyNum[,c(7:12)] = as.data.frame(copMat)
 
-head(bedLike)
+write.table(copyNum, "141209_bedLike.txt", sep='\t')
+
+seqNames = as.character(copyNum$chromosome_name)
 gr = GRanges(seqnames = Rle(seqNames),
-            ranges = IRanges(start=bedLike$start_position, end=bedLike$end_position),
-            gem = bedLike[,c(7:12)],
-            name = bedLike$external_gene_name)
+            ranges = IRanges(start=copyNum$start_position, end=copyNum$end_position),
+            gem = copyNum[,c(7:12)],
+            name = copyNum$external_gene_name)
 gr
+
+ggplot(data=copyNum, aes(x=begining, y=GIC_011)) + geom_line() + geom_point() + theme_bw(base_size=24)
