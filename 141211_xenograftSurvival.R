51d9d988de30c53b64a047f3657a2196574c625d
dvbrown <dvbrown@student.unimelb.edu.au>
2014-12-11 15:24:08 +1100

    end of script commit before H & E

--- 141211_xenograftSurvival.R
+++ 141211_xenograftSurvival.R
@@ -6,16 +6,35 @@ library(ggplot2)
 setwd("~/Documents/Xenograft/")
 theData = read.delim("141211_mouseSurvival.txt")
 
+theDate = Sys.Date()
+
 colors = c("darkblue","orange", "red", "darkgreen", "yellow")
 names(colors) = levels(theData$Sample)
 
 data.surv = Surv(theData$Survival, event=theData$Event)
 sur.fit = survfit(data.surv ~ Sample, theData)
 
-plot(sur.fit, main='TCGA GBM cohort all patients classified by subtype',ylab='Survival probability', xlab='survival (days)', 
+setwd("~/Documents/Xenograft/results/")
+pdf(file=paste(theDate, "kmPlot.pdf", sep="_"), paper="a4r", useDingbats=F)
+
+plot(sur.fit, main='Failed xenograft experiment',ylab='Survival probability', xlab='survival (days)', 
      col=colors,
      cex=1.75, conf.int=F, lwd=1.33, cex.axis=1.5, cex.lab=1.5)
 
-legend('topright', names(colors), title="Coexpression subtype",
+legend('bottomleft', names(colors), title="Coexpression subtype",
        col=colors,
-       lwd=1.33, cex=1.75, bty='n', xjust=0.5, yjust=0.5)
+       lwd=1.33, cex=1.25, bty='n', xjust=0.5, yjust=0.5)
+dev.off()
+
+p = ggplot(theData, aes(Sample, Survival, fill=Sample)) +
+    geom_boxplot() + geom_jitter() + scale_fill_manual(values=colors) +
+    xlab("PDGC") + ylab("Survival (days)") +
+    ggtitle("Failed xenograft experiment") + theme_bw(base_size=16) + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1), text = element_text(size=24))
+
+
+setwd("~/Documents/Xenograft/results/")
+pdf(file=paste(theDate, "boxPlot.pdf", sep="_"), paper="a4r", useDingbats=F)
+p
+dev.off()
+
