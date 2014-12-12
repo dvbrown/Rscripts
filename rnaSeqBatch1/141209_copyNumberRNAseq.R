608672b724238522528b820bbe111eb785e084d0
dvbrown <dvbrown@student.unimelb.edu.au>
2014-12-12 16:08:55 +1100

    got the script to print good output for copy number

--- rnaSeqBatch1/141209_copyNumberRNAseq.R
+++ rnaSeqBatch1/141209_copyNumberRNAseq.R
@@ -16,28 +16,29 @@ annotateIds = function(geneList)  {
     return (list)
 }
 
-moveAv <- function(x,n=100){
+moveAv <- function(x,n=800){
     # X is the time series a numeric vector
     # n is the window size to use. 100 is from Patel et al 2014 single cell GBM
+    # Multily by 8 since I HAVE 8 times as many genes
     return (filter(x,rep(1/n,n), sides=2, circular=F))
 }
 
 setwd("~/Documents/RNAdata/danBatch1/bowtieGem/revHTSeq/copyNumber/")
 
 ####################### Generate a bed like file ############################
-# cpm = read.delim("../GLMedgeR/131021_normalisedCPM.txt", row.names=1)
-# head(cpm)
-# 
-# # Load the genomic coordinates in a bed like format
-# mart<- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
-# 
-# bedLike = annotateIds(cpm)
-# head(bedLike)
-# bedCpm = merge.data.frame(bedLike, cpm, by.x="ensembl_gene_id", by.y=0)
-# write.table(bedCpm, "141209_bedLike.txt", sep='\t')
+cpm = read.delim("../GLMedgeR/131021_normalisedCPM.txt", row.names=1)
+head(cpm)
+
+# Load the genomic coordinates in a bed like format
+mart<- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+bedLike = annotateIds(cpm)
+head(bedLike)
+bedCpm = merge.data.frame(bedLike, cpm, by.x="ensembl_gene_id", by.y=0)
+write.table(bedCpm, "141212_bedLike.txt", sep='\t')
 
 #################### Center the expression levels and take the moving average ######################
-bedLike = read.delim("141209_bedLike.txt")
+bedLike = read.delim("141212_bedLike.txt")
 # Subset only vlaid chromosome names
 chr = as.factor(c(1:22, "X", "Y"))
 bedLike = bedLike[bedLike$chromosome_name %in% chr,]
@@ -58,48 +59,50 @@ copyNum$begining = paste(copyNum$chromosome_name, copyNum$start_position, sep="_
 # Extract and z center the copy number
 copMat = as.matrix(copyNum[,c(7:12)])
 copMat = scale(copMat)
+# Remove NA columns
 copyNum[,c(7:12)] = as.data.frame(copMat)
+copyNum = copyNum[c(400:48112),]
 
-write.table(copyNum, "141209_bedLike.txt", sep='\t')
+write.table(copyNum, "141212_bedLike_average800.txt", sep='\t')
 
 #################### Draw the copy number plot #######################
-copyNum = read.delim("141209_bedLike.txt")
-copyNum$chromosome_name = paste("chr", copyNum$chromosome_name, sep="")
+copyNum = read.delim("141212_bedLike_average800.txt")
+copyNum$chromosome_name = paste("hs", copyNum$chromosome_name, sep="")
 
-myLengths =  c(249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663,
-                146364022, 141213431, 135534747, 135006516, 133851895, 115169878, 107349540, 
-                102531392,  90354753,  81195210,  78077248,  59128983,  63025520,  48129895, 
-                51304566, 155270560,  59373566)
+# myLengths =  c(249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663,
+#                 146364022, 141213431, 135534747, 135006516, 133851895, 115169878, 107349540, 
+#                 102531392,  90354753,  81195210,  78077248,  59128983,  63025520,  48129895, 
+#                 51304566, 155270560,  59373566)
 
 # Construct a very long bed file by row binding indivdual samples
 wantedColumns = c("chromosome_name", "start_position", "end_position", "external_gene_name")
 
 copyNumber = copyNum[,c("chromosome_name", "start_position", "end_position", "external_gene_name",
                         'GIC_011', 'GIC_020', 'GIC_034', 'GIC_035', 'GIC_039', 'GIC_041')]
-write.table(copyNumber, "141210_bedFile.bed", sep='\t', row.names=F)
+write.table(copyNumber, "141212_bedFile.bed", sep='\t', row.names=F)
 
-seqNames = as.character(copyNum$chromosome_name)
+seqNames = as.factor(copyNum$chromosome_name)
 
 # Write indivdual sample files
-copyN = read.delim("141210_bedFile.bed")
-copyN$chromosome_name = gsub("chr", "hs", copyN$chromosome_name)
-copyN = copyN[c(50:48512),c(1:3,5:10)]
+copyN = read.delim("141212_bedFile.bed")
+copyN = copyN[,c(1:3,5:10)]
+copyN$chromosome_name = as.factor(copyN$chromosome_name)
 
-colnames(copyN)
+head(copyN)
 gic_011 = copyN[,c(1:4)]
-write.table(gic_011, "gic_011.txt", sep='\t', row.names=F)
+write.table(gic_011, "gic_011.txt", sep='\t', row.names=F, col.names=F, quote=F)
 
 gic_020 = copyN[,c(1:3,5)]
-write.table(gic_020, "gic_020.txt", sep='\t', row.names=F)
+write.table(gic_020, "gic_020.txt", sep='\t', row.names=F, col.names=F, quote=F)
 
 gic_034 = copyN[,c(1:3,6)]
-write.table(gic_034, "gic_034.txt", sep='\t', row.names=F)
+write.table(gic_034, "gic_034.txt", sep='\t', row.names=F, col.names=F, quote=F)
 
 gic_035 = copyN[,c(1:3,7)]
-write.table(gic_035, "gic_035.txt", sep='\t', row.names=F)
+write.table(gic_035, "gic_035.txt", sep='\t', row.names=F, col.names=F, quote=F)
 
 gic_039 = copyN[,c(1:3,8)]
-write.table(gic_039, "gic_039.txt", sep='\t', row.names=F)
+write.table(gic_039, "gic_039.txt", sep='\t', row.names=F, col.names=F, quote=F)
 
 gic_041 = copyN[,c(1:3,9)]
-write.table(gic_041, "gic_041.txt", sep='\t', row.names=F)
+write.table(gic_041, "gic_041.txt", sep='\t', row.names=F, col.names=F, quote=F)
