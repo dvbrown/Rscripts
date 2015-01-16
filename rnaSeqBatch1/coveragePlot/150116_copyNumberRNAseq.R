f68d20ec7df38acc54e73b48b4638064ad96f6c3
dvbrown <dvbrown@student.unimelb.edu.au>
2015-01-16 15:50:13 +1100

    finished heat map

--- rnaSeqBatch1/coveragePlot/150116_copyNumberRNAseq.R
+++ rnaSeqBatch1/coveragePlot/150116_copyNumberRNAseq.R
@@ -1,4 +1,6 @@
 # Draw coverage plot according to http://davetang.org/muse/2013/09/07/creating-a-coverage-plot-in-r/
+library(gplots)
+library(RColorBrewer)
 
 setwd("~/Documents/RNAdata/danBatch1/bowtieGem/revHTSeq/copyNumber/")
 list.files()
@@ -6,12 +8,12 @@ list.files()
 totCov = read.delim("141212_bedLike_average800.txt")
 
 # Extract chr 7
-smallCov = totCov[totCov$chromosome_name %in% 7,c(4:12)]
-plot(smallCov$GIC_011, type="l",
+chr7 = totCov[totCov$chromosome_name %in% 7,c(4:12)]
+plot(chr7$GIC_011, type="l",
      ylim = c(-4,4),
      main = "Coverage plot", ylab = "Copy number",
      xlab = "Chromosome 7", col = 'blue', lwd=2.5)
-lines(smallCov$GIC_020, lwd=2.5, col = 'red')
+lines(chr7$GIC_020, lwd=2.5, col = 'red')
 
 # Try the same fpr chromosome 10
 chr10 = totCov[totCov$chromosome_name %in% 10,c(4:12)]
@@ -21,4 +23,31 @@ plot(chr10$GIC_011, type="l",
      xlab = "Chromosome 10", col = 'blue', lwd=2.5)
 lines(chr10$GIC_020, lwd=2.5, col = 'red')
 
-########### Try out the heatmp method instead ###############
+########### Test the heatmp method instead ###############
+myPalette <- colorRampPalette(c("blue", "white", "red"))(n = 1000)
+
+# Make heat map with copyNumber info
+smallCov= rbind(totCov[c(1:200),],totCov[c(20000:20200),])
+matCov = t(as.matrix(smallCov[,c(7:12)]))
+
+# Retreive the indicies of the first occurance of the chromosome names for use in colsep
+colSep = match(as.factor(c(1,17, 18)), smallCov$chromosome_name)
+
+heatmap.2(matCov, cexRow=1.5, main="Copy number as inferred by RNA-seq",
+          colsep=colSep, sepcolor="black", sepwidth=c(2,2),
+          keysize=1, trace="none", key.title="CNV",
+          col=myPalette, density.info="none", dendrogram="row", 
+          labRow=row.names(matCov), xlab="Position",
+          offsetRow=c(1,1), margins=c(2,7.5))
+
+########### Full heatmap ###############
+matCov = t(as.matrix(totCov[,c(7:12)]))
+
+colSep = match(as.factor(c(1:22, "X", "Y")), totCov$chromosome_name)
+
+heatmap.2(matCov, cexRow=1.5, main="Copy number as inferred by RNA-seq",
+          colsep=colSep, sepcolor="black", sepwidth=c(2,2),
+          keysize=1, trace="none", key.title="CNV",
+          col=myPalette, density.info="none", dendrogram="row", 
+          labRow=row.names(matCov), xlab="Position",
+          offsetRow=c(1,1), margins=c(2,7.5))
