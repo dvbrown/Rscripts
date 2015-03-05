0ed60d6c3a3cb36cc2488c9488e090b6ece5c323
dvbrown <dvbrown@student.unimelb.edu.au>
2015-03-05 16:10:45 +1100

    new analysis of facs

---
+++ 150305_facsStupp.R
@@ -0,0 +1,6 @@
+# A script to analyse the FACS experiment that I have done
+
+setwd("~/Documents/facsData/flowJo/fortessa/150302_stupp/data/")
+list.files()
+
+cellCycle = read.delim("150305_cellCycleMunged.txt")
