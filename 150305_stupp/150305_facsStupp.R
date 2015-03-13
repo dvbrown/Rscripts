c5358422bffcf9641acc77fb11b822ec02e08bcd
dvbrown <dvbrown@student.unimelb.edu.au>
2015-03-13 15:03:14 +1100

    added script for replicates

--- 150305_stupp/150305_facsStupp.R
+++ 150305_stupp/150305_facsStupp.R
@@ -1,7 +1,17 @@
 # A script to analyse the FACS experiment that I have done
 require(ggplot2)
 library(reshape)
+library(plyr)
 source("~/Documents/Rscripts/multiplot.R")
+
+subtractBaseline = function(dataFrame, baseline, value) {
+    dataSorted = dataFrame[order(dataFrame[,'PDGC'], dataFrame[,'Subpopulation']),]
+    baseline = dataSorted[dataSorted$Subpopulation %in% baseline,]
+    treatment = dataSorted[dataSorted$Subpopulation %in% value,]
+    subtract =  treatment[,c(5:7)] - baseline[,c(5:7)]
+    result = cbind(treatment[,c(1,2)], subtract)
+    return (result)
+}
 
 setwd("~/Documents/facsData/flowJo/fortessa/150302_stupp/data/")
 list.files()
@@ -49,3 +59,37 @@ MU004 = ggplot(data=cellCycleLong[cellCycleLong$PDGC %in% "MU004",],
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 multiplot(MU020, MU035, MU039, MU004, cols=2)
+
+bioRep = ddply(cellCycleLong, .(Subpopulation, variable), summarise, meanPhase = mean(value, na.rm=T), 
+               sdPhase = sd(value, na.rm=T),reps=length(value))
+bioRep$seDdCt = bioRep$sdPhase / (sqrt(bioRep$reps))
+
+absolute = ggplot(data=bioRep, aes(x=Subpopulation, y=meanPhase, fill=variable)) +
+            geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+            ggtitle("qPCR biological Replicates (n = 2 - 3)") +  scale_fill_manual(values=c("lightblue", "gold1", "orangered")) + 
+            geom_errorbar(aes(ymin=meanPhase-seDdCt, ymax=meanPhase+seDdCt), width=.2, position=position_dodge(0.9)) +
+            xlab("Gene") + ylab("ddCt") +
+            theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#### Present the data relative to double negative ####
+
+diff = rbind(subtractBaseline(cellCycle, "CD44-/CD133-", "CD44-/CD133-"),
+             subtractBaseline(cellCycle, "CD44-/CD133-", "CD44+/CD133-"),
+             subtractBaseline(cellCycle, "CD44-/CD133-", "CD44-/CD133+"),
+             subtractBaseline(cellCycle, "CD44-/CD133-", "CD44+/CD133+"))
+
+cellCycleLong = melt(diff, id.vars=c("PDGC", "Subpopulation"))
+
+# Summarise the data by biological replicate
+cellCycleLong = cellCycleLong[!cellCycleLong$PDGC %in% "MU039"]
+bioRep = ddply(cellCycleLong, .(Subpopulation, variable), summarise, meanPhase = mean(value, na.rm=T), 
+               sdPhase = sd(value, na.rm=T),reps=length(value))
+bioRep$seDdCt = bioRep$sdPhase / (sqrt(bioRep$reps))
+
+baseline = ggplot(data=bioRep, aes(x=variable, y=meanPhase, fill=Subpopulation)) +
+            geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+            ggtitle("Cell cycle profile of PDGCs by subpopulation") +  scale_fill_manual(values=c("forestgreen", "lightblue", "gold1", "red")) + 
+            geom_errorbar(aes(ymin=meanPhase-seDdCt, ymax=meanPhase+seDdCt), width=.2, position=position_dodge(0.9)) +
+            xlab("Gene") + ylab("ddCt") +
+            theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+baseline
