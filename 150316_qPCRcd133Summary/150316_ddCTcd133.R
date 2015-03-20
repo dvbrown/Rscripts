1e287a0a43f3720f9de4f8542dc133ac689846dd
dvbrown <dvbrown@student.unimelb.edu.au>
2015-03-21 09:07:43 +1100

    inspecting data values

--- 150316_qPCRcd133Summary/150316_ddCTcd133.R
+++ 150316_qPCRcd133Summary/150316_ddCTcd133.R
@@ -105,19 +105,34 @@ mu030a_P = getddCt(datWide, "MU030a_CD133_pos")
 mu041_P = getddCt(datWide, "MU041_CD133_pos")
 
 ddCt = rbind(mu011_P, mu020_P, mu030_P, mu030a_P, mu035_P, mu041_P)
-write.csv(ddCt,"150317_ddCtResults.csv", row.names=F)
+# write.csv(ddCt,"150317_ddCtResults.csv", row.names=F)
 
 rm(mu011_P, mu020_P, mu030_P, mu030a_P, mu035_P, mu041_P)
 # Get rid of the genes only measured on ES cells
-ddCt = ddCt[!ddCt$Gene %in% c("ATP5G3", "B2M", "CREB1", "GAPDH", "GRIA2",
+ddCt = ddCt[!ddCt$Gene %in% c("ATP5G3", "B2M", "CREB1",  "GRIA2", #"GAPDH",
                               "HAPLN1", "MGMT", "REST"),]
 
 orderedDat = ddCt[order(ddCt$Gene, ddCt$PDGC),]
 
 # Make a boxplot of ddCT valuse
 ggplot(data=ddCt, aes(x=Gene, y=ddCT)) +
-    geom_boxplot() + geom_point(aes(shape=Subpopulation, colour=Sample), size =3, alpha=0.7,  position = position_jitter(w = 0.2)) + #geom_jitter() +
+    geom_boxplot() + geom_point(aes(colour=Sample), size =3, alpha=0.7,  position = position_jitter(w = 0.2)) +
     ggtitle("qPCR Summary") + geom_hline(yintercept=0, colour="red") +
     xlab("Gene") + ylab("ddCt relative to CD133 negative") +
     scale_y_continuous(breaks = round(seq(-6, 6, by = 1),1)) +
+    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+######## Summarise by biological replicates ######## 
+
+bioRep = ddply(ddCt, .(Subpopulation, Gene), summarise, meanddCt = mean(ddCT, na.rm=T), 
+               sdddCt = sd(ddCT, na.rm=T), reps=length(ddCT))
+bioRep$seddCt = bioRep$sdddCt / (bioRep$reps)
+write.csv(bioRep, "150321_biologicalRepCD133.csv")
+
+# Plot the biological replicates
+ggplot(data=bioRep, aes(x=Gene, y=meanddCt, fill=Subpopulation)) +
+    geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+    ggtitle("qPCR biological Replicates (n = 5 - 6)") +  scale_fill_manual(values=c("blue", "gold1")) + 
+    geom_errorbar(aes(ymin=meanddCt-seddCt, ymax=meanddCt+seddCt), width=.2, position=position_dodge(0.9)) +
+    xlab("Gene") + ylab("ddCT relative to H9 NSC") +
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
