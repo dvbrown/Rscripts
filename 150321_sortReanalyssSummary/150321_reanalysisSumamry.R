565b7e4e48fe3f4b566a9c33f8150576baa24b66
dvbrown <dvbrown@student.unimelb.edu.au>
2015-03-21 14:16:55 +1100

    almost happy with analysis

--- 150321_sortReanalyssSummary/150321_reanalysisSumamry.R
+++ 150321_sortReanalyssSummary/150321_reanalysisSumamry.R
@@ -1,5 +1,6 @@
 # Thge script wll summarise the 2 experiments I did that sorted and then reanalysed PDGCs
 library(ggplot2)
+library(plyr)
 source("~/Documents/Rscripts/multiplot.R")
 
 subtractBaseline = function(dataFrame, baseline, value) {
@@ -50,3 +51,42 @@ mu039Result = cbind(rbind(dat["061114_039_CD44.fcs", c(1:3)], dat["061114_039_DN
 
 freq = rbind(freq1, freq2, mu039Result)
 freq = freq[order(freq[,'PDGC'], freq[,'Subpopulation']),]
+
+# convert to long
+diff = melt(freq, id.vars=c("PDGC", "Subpopulation", "Date"))
+
+######## Summarise by biological replicates ######## 
+cols = c("gold", "red", "blue", "forestgreen")
+bioRep = ddply(diff, .(PDGC, variable, Subpopulation), summarise, meanDiff = mean(value, na.rm=T), 
+               sdDiff = sd(value, na.rm=T), reps=length(value))
+bioRep$seDiff = bioRep$sdDiff / (bioRep$reps)
+write.csv(bioRep, "150321_bioReps.csv")
+
+ggplot(data=bioRep[bioRep$PDGC %in% "MU035",], 
+       aes(x=Subpopulation, y=meanDiff, fill=variable)) +
+    geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+    ggtitle("MU035 recurrent GBM n = 2") +  scale_fill_manual(values=cols) + 
+    geom_errorbar(aes(ymin=meanDiff-seDiff, ymax=meanDiff+seDiff), width=.2, position=position_dodge(0.9)) +
+    xlab("Sorted population") + ylab("% difference to mixed population") + scale_y_continuous(breaks = round(seq(-50, 50, by = 10),1)) +
+    theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# Replicate across all PDGCs, EXCEPT for MU035 which is an outlier
+pdgcRep = ddply(diff[!diff$PDGC %in% "MU035",], .(variable, Subpopulation), summarise, meanDiff = mean(value, na.rm=T), 
+                sdDiff = sd(value, na.rm=T), reps=length(value))
+pdgcRep$seDiff = pdgcRep$sdDiff / sqrt(pdgcRep$reps)
+
+ggplot(data=pdgcRep, aes(x=Subpopulation, y=meanDiff, fill=variable)) +
+    geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+    ggtitle("All PDGCs except MU035 \nn = 3 - 4") +  scale_fill_manual(values=cols) + 
+    geom_errorbar(aes(ymin=meanDiff-seDiff, ymax=meanDiff+seDiff), width=.2, position=position_dodge(0.9)) +
+    xlab("Sorted population") + ylab("% difference to mixed population") + scale_y_continuous(breaks = round(seq(-50, 50, by = 10),1)) +
+    theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+#### Make a boxplot of difference ####
+ggplot(data=diff, aes(x=variable, y=value)) +
+    geom_boxplot() + geom_point(aes(colour=Subpopulation), size =3, alpha=0.7,  position = position_jitter(w = 0.2)) +
+    ggtitle("qPCR Summary") + geom_hline(yintercept=0, colour="red") +
+    xlab("Gene") + ylab("ddCt relative to CD133 negative") +
+    scale_y_continuous(breaks = round(seq(-50, 50, by = 10),1)) +
+    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
