b2b8dec3e415952f393654ff0079b69014d8c194
dvbrown <dvbro0@gmail.com>
2015-06-13 11:48:58 +1000

    done MU020

--- 150612_hyperHypoxia/hyperHypoxia.R
+++ 150612_hyperHypoxia/hyperHypoxia.R
@@ -11,17 +11,29 @@ dat = dat_all[,c(1,2,4:7)]
 
 # Convert from wide to long
 mDat = melt(dat, id.vars = c("PDGC", "Treatment"))
+mDat = mDat[!mDat$Treatment %in% "isotype",]
 
 # Biological replicates
-bioRep = ddply(diff, .(PDGC, variable, Subpopulation), summarise, meanDiff = mean(value, na.rm=T), 
+bioRep = ddply(mDat, .(PDGC, Treatment, variable), summarise, meanValue = mean(value, na.rm=T), 
                sdDiff = sd(value, na.rm=T), reps=length(value))
 bioRep$seDiff = bioRep$sdDiff / (bioRep$reps)
 
+#### Barchart ####
+# Plot the biological replicates
+mu020 = ggplot(data=bioRep[bioRep$PDGC %in% "MU020",], 
+              aes(x=variable, y=meanValue, fill=Treatment)) +
+    geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+    ggtitle("Reanalysis biological Replicates n = 3") +  
+    scale_fill_manual(values=c("orange", "darkred", "darkblue", "forestgreen")) + 
+    geom_errorbar(aes(ymin=meanValue-seDiff, ymax=meanValue+seDiff), width=.2, position=position_dodge(0.9)) +
+    xlab("Subpopulation") + ylab("Percent difference \nrelative to mixed population") +
+    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+mu020
 
 ### Boxplot ###
-box = ggplot(data=dat, aes(x=PDGC, y=ddCT)) +
-    geom_boxplot() + geom_point(aes(colour=PDGC), size =3, alpha=0.7,  position = position_jitter(w = 0.175)) +
-    ggtitle("qPCR Summary") + geom_hline(yintercept=0, colour="red") +
+box = ggplot(data=mDat, aes(x=PDGC, y=value)) +
+    geom_boxplot() + geom_point(aes(colour=Treatment), size =3, alpha=0.7,  position = position_jitter(w = 0.175)) +
+    ggtitle("qPCR Summary") +
     xlab("Gene") + ylab("ddCt relative to CD133 negative") +
     scale_y_continuous(breaks = round(seq(-6, 6, by = 1),1)) +
     theme_bw(base_size=14) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
