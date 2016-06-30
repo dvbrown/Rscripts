1ef1ff1c6ba979b9f296f2e1d2d1a315ef6f3d03
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-06-30 14:43:38 +0200

    made batch version

--- Postdoc/GandT/160606_Validation/160606_logR_segmentation.R
+++ Postdoc/GandT/160606_Validation/160606_logR_segmentation.R
@@ -82,3 +82,73 @@ for(gamma in c(25))
 
 
    }
+
+
+
+
+######### Batch version ########
+for (item in samplelist) {
+  sampletostudy = as.character(samplelist[item])
+
+  setwd(writedir)
+  for(gamma in c(25))
+  {
+    
+    setwd(root)    
+    thresholds <- paste("-PCF-kmin_3-gamma_",gamma,"-fastPCF-window-",window,"vs-NoREF-M30-II",sep="")
+    
+    scdata <- read.delim(sampletostudy,sep="")
+    
+    chrom <- c(seq(1,22),"X","Y")
+    SEGMENTATIONtable <- vector("list",length(chrom))
+    
+    
+    for(chr in 1:23)
+    {
+      print(paste(type," chr ",chr," of binsize ",binsize," ",sampletostudy,sep=""))
+      pos <- scdata$position[as.character(scdata$chromosome)==chrom[chr]]
+      posend <- scdata$end[as.character(scdata$chromosome)==chrom[chr]]
+      posabs <- scdata$abs[as.character(scdata$chromosome)==chrom[chr]]
+      data <- scdata$log2[as.character(scdata$chromosome)==chrom[chr]]
+      y <- data[order(pos)][is.finite(data[order(pos)])]
+      posselect <- pos[order(pos)][is.finite(data[order(pos)])]
+      posendselect <- posend[order(posend)][is.finite(data[order(posend)])]
+      posabsselect <- posabs[order(posabs)][is.finite(data[order(posabs)])]
+      
+      sdev=getMad(y,k=25)
+      res=selectFastPcf(y,3,gamma*sdev,T)
+      segments=res$yhat
+      
+      SEGMENTATIONtable[[chr]] <- cbind(rep(chrom[chr],length(posselect)), posselect, y, segments,posendselect,posabsselect)
+    }
+    
+    towriteSEGMENTS <- do.call("rbind", SEGMENTATIONtable)
+    colnames(towriteSEGMENTS) <- c("Chr","Pos","logR","logRsegment","end","abs")
+    
+    setwd(writedir)
+    write.table(towriteSEGMENTS, paste("SEGMENTSlogR.GCcorrected-M30-",as.character(samplelist[item]),
+                                       thresholds,type,".txt",sep=""), quote=F,sep="\t",col.names=T,row.names=F)
+    
+    #######################################
+    ### plotting logR SEGMENTATION
+    setwd(writedir)
+    for(chr in 1:23){
+      
+      jpeg(paste(sampletostudy,"Complete_chr_logR.gamma",gamma,".",type,binsize,"_",chr,".jpeg",sep=""),
+           width = 1200, height = 600, units = "px", pointsize = 20)		#,width=10,height=10)
+      #par(mfrow=c(2,1))
+      limx <- max(as.numeric(as.character(towriteSEGMENTS[as.character(towriteSEGMENTS[,1])==chrom[chr],2]))[is.finite(as.numeric(as.character(towriteSEGMENTS[as.character(towriteSEGMENTS[,1])==chrom[chr],2])))])
+      
+      
+      plot(as.numeric(as.character(towriteSEGMENTS[as.character(towriteSEGMENTS[,1])==chrom[chr],2])),
+           as.numeric(as.character(towriteSEGMENTS[as.character(towriteSEGMENTS[,1])==chrom[chr],3])),col="black",
+           pch=19,ylim=c(-4,4),xlim=c(1,limx),cex=0.5, xlab=NA,ylab=paste("logR (chr ",chrom[chr],") ",sep=""),
+           main=paste(as.character(samplelist[item]),".",type,binsize,".gamma",gamma,sep=""),cex.main=1)
+      
+      points(as.numeric(as.character(towriteSEGMENTS[as.character(towriteSEGMENTS[,1])==chrom[chr],2])),
+             as.numeric(as.character(towriteSEGMENTS[as.character(towriteSEGMENTS[,1])==chrom[chr],4])),
+             col="yellow",pch=19,cex=0.7)
+      dev.off()
+    }
+  }
+}
