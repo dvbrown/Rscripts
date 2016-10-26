8919709a8e44c7e8b410a09fa2e5673c03364ec3
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-10-26 11:21:14 +0200

    finished QC

--- Postdoc/GandT/161026_mariaQC/libSize.R
+++ Postdoc/GandT/161026_mariaQC/libSize.R
@@ -9,7 +9,7 @@ setwd(inputDir)
 
 # Read in samplesheet
 samplesheet = read.csv("sampleSheetAll.csv", row.names = 1)
-samplesheet = samplesheet[,c(1:4,6:8)]
+samplesheet = samplesheet[,c(1:4,6:8,9)]
 rownames(samplesheet) = samplesheet[,5]
 
 # Read in individaual batch GEMs
@@ -27,4 +27,25 @@ rm(b1, b2, b3, b4)
 libSize = as.data.frame(colSums(count))
 row.names(libSize)
 row.names(samplesheet)
+# Calculate non-zero genes
+geneSize =  as.data.frame(colSums(count != 0))
+libSize = cbind(libSize, geneSize)
+colnames(libSize) = c("LibSize", "NonZero")
+
 libMatrix = merge.data.frame(samplesheet, libSize, by.x = 0, by.y = 0)
+
+m = ggplot(libMatrix, aes(x=sample_ID, y=LibSize))  + geom_boxplot() +
+  xlab("Sample") + ylab("Library Size") + # Set axis labels
+  ggtitle("SMART-Seq lib size") + geom_jitter(aes(colour=Batch, width = 12),size=1.5)  +
+  theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  theme(text = element_text(size=20)) + guides(colour=FALSE)
+m
+
+n = ggplot(libMatrix, aes(x=sample_ID, y=NonZero))  + geom_boxplot() +
+  xlab("Sample") + ylab("Genes with non-zero counts") + # Set axis labels
+  ggtitle("SMART-Seq non-zero") + geom_jitter(aes(colour=Batch, width = 12),size=1.5)  +
+  theme_bw(base_size=18) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  theme(text = element_text(size=20)) + guides(colour=FALSE)
+n
+
+multiplot(m, n, cols=2)
