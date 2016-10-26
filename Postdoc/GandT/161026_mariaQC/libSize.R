73dc560f26474ffaa0e7f7806573abc8d2d6fef8
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-10-26 09:57:21 +0200

    made sample sheet

--- Postdoc/GandT/161026_mariaQC/libSize.R
+++ Postdoc/GandT/161026_mariaQC/libSize.R
@@ -7,18 +7,24 @@ inputDir = "/Users/u0107775/Data/GandT_Seq/161020_MariaXydia/Counts_Maria/"
 exportDir = "/Users/u0107775/Data/GandT_Seq/161020_MariaXydia/"
 setwd(inputDir)
 
+# Read in samplesheet
 samplesheet = read.csv("sampleSheetAll.csv", row.names = 1)
 samplesheet = samplesheet[,c(1:4,6:8)]
 rownames(samplesheet) = samplesheet[,5]
 
+# Read in individaual batch GEMs
 b1 = read.csv("Batch1/b1_htseq_counts_all.csv", row.names = 1)
 b2 = read.csv("Batch2/b2_htseq_counts_all.csv", row.names = 1)
 b3 = read.csv("Batch3/b3_htseq_counts_all.csv", row.names = 1)
 b4 = read.csv("Batch4/b4_htseq_counts_all.csv", row.names = 1)
 
+# Bind GEMS
 count = cbind(b1, b2, b3, b4)
 rm(b1, b2, b3, b4)
-write.table(count, "161026_geneExpMatrix.txt", sep="\t")
+#write.table(count, "161026_geneExpMatrix.txt", sep="\t")
 
+# Calculate library size and merge on sample sheet
 libSize = as.data.frame(colSums(count))
-libMatrix = merge(count, libSize)
+row.names(libSize)
+row.names(samplesheet)
+libMatrix = merge.data.frame(samplesheet, libSize, by.x = 0, by.y = 0)
