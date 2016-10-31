2c60ee81a1f9babd2eaa81b0aaab894098286421
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-10-31 13:39:58 +0100

    added heat map to ggplot

--- Templates/ggplot2.R
+++ Templates/ggplot2.R
@@ -1,8 +1,4 @@
 library(ggplot2)
-library(reshape)
-library(plyr)
-
-
 
 #### Barchart ####
 plt = ggplot(data=bioRep[bioRep$PDGC %in% "MU020",], 
@@ -15,9 +11,20 @@ plt = ggplot(data=bioRep[bioRep$PDGC %in% "MU020",],
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(text = element_text(size=20))
 plt
 
-# Plot correlation of the replicate
+#### Plot correlation of the replicate ####
 correlation = ggplot(data=mungedData, aes(x=Cp.y, y=Cp.x, color=gene.x)) + 
   geom_point(shape=19) + geom_smooth(method=lm, colour='red') +
   xlab("Replicate 1") + ylab("Replicate 2") + # Set axis labels
   ggtitle("Correlation of technical replicates") +  # Set title
   theme_bw(base_size=18)
+
+
+#### Plot some metric generated from 96 well plate into a 96 well matrix ####
+# Insert well positions
+wellMap = data.frame (rown = rep (letters[1:8], each=12), coln = rep (1:12, 8))
+platelay = cbind(wellMap, dat)
+
+# Change the aesthetics according to dataset
+ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
+  geom_point(aes(colour = GenesDetected, size= LibSize))  + theme_bw() + scale_size(range = c(5, 20)) +
+  labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red")
