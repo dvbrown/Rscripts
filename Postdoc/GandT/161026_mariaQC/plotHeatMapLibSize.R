65e0d1ebff5854bd38c3181c3450aea04dfeec3e
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-11-01 11:41:06 +0100

    extracted to function

--- Postdoc/GandT/161026_mariaQC/plotHeatMapLibSize.R
+++ Postdoc/GandT/161026_mariaQC/plotHeatMapLibSize.R
@@ -1,6 +1,24 @@
 library(ggplot2)
-library(RColorBrewer)
 source('~/Code/Rscripts/Templates/multiplot.R')
+
+plot96wellMap <- function (sampleSubset, variable_2_colour, variable_2_size) {
+  # DATAFRAME    sampleSubset = the 96 samples you want to plot. This must be in order with well A12 = sample 12 and well B1 = sample 13
+  # STRING     variable_2_colour = the measurement you want mapped to colour
+  # STRING     variable_2_size = the measurement you want mapped to circle size
+  
+  wellMap = data.frame (rown = rep (LETTERS[1:8], each=12), coln = rep (1:12, 8))
+  platelay = cbind(wellMap, sampleSubset)
+  
+  # ggplot using well coordinates generate dabove
+  plt = ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
+    geom_point(aes_string(colour = variable_2_colour, size= variable_2_size)) + scale_size(range = c(5, 10)) +
+    labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red") +
+    theme_bw() + theme(axis.text=element_text(size=16))
+  return(plt)
+}
+
+b1 = plot96wellMap(batch1, "GenesDetected", "LibSize")
+b1
 
 inputDir = "/Users/u0107775/Data/GandT_Seq/161020_MariaXydia/Counts_Maria/"
 exportDir = "/Users/u0107775/Data/GandT_Seq/161020_MariaXydia/"
@@ -18,12 +36,42 @@ datMerge <- datMerge[order(datMerge$sample_number),]
 
 # Get the first batch of samples sample_number 1 - 96
 batch1 = datMerge[c(1:96),]
+batch2 = datMerge[c(97:192),]
+batch3 = datMerge[c(193:288),]
+batch4 = datMerge[c(289:384),]
 
-# Insert well positions
-wellMap = data.frame (rown = rep (letters[1:8], each=12), coln = rep (1:12, 8))
+# Insert well positions batch 1
+wellMap = data.frame (rown = rep (LETTERS[1:8], each=12), coln = rep (1:12, 8))
 platelay = cbind(wellMap, batch1)
-
 # ggplot using well coordinates
-ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
-  geom_point(aes(colour = GenesDetected, size= LibSize))  + theme_bw() + scale_size(range = c(5, 20)) +
-  labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red")
+b1 = ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
+    geom_point(aes(colour = GenesDetected, size= LibSize)) + scale_size(range = c(5, 10)) +
+    labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red") +
+    theme_bw() + theme(axis.text=element_text(size=16))
+
+# Insert well positions batch 2
+platelay = cbind(wellMap, batch2)
+# ggplot using well coordinates
+b2 = ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
+  geom_point(aes(colour = GenesDetected, size= LibSize)) + scale_size(range = c(5, 10)) +
+  labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red") +
+  theme_bw() + theme(axis.text=element_text(size=16))
+
+# Insert well positions batch 3
+platelay = cbind(wellMap, batch3)
+# ggplot using well coordinates
+b3 = ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
+  geom_point(aes(colour = GenesDetected, size= LibSize)) + scale_size(range = c(5, 10)) +
+  labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red") +
+  theme_bw() + theme(axis.text=element_text(size=16))
+
+# Insert well positions batch 4
+platelay = cbind(wellMap, batch4)
+# ggplot using well coordinates
+b4 = ggplot(platelay, aes(y = factor(rown, rev(levels(rown))),x = factor(coln))) + 
+  geom_point(aes(colour = GenesDetected, size= LibSize)) + scale_size(range = c(5, 10)) +
+  labs(x=NULL, y = NULL) + scale_color_gradient(low="blue", high="red") +
+  theme_bw() + theme(axis.text=element_text(size=16))
+
+# PLOT ALL PLATES
+multiplot(b1, b2, b3, b4, cols = 2)
