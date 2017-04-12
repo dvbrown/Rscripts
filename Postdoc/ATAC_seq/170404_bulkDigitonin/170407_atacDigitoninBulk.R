3a6241058b7a8c07761fd965116dde44a7bfbf78
Daniel <daniel.brown@cme.vib-kuleuven.be>
2017-04-12 10:46:35 +0200

    made plots for symbiosys

--- Postdoc/ATAC_seq/170404_bulkDigitonin/170407_atacDigitoninBulk.R
+++ Postdoc/ATAC_seq/170404_bulkDigitonin/170407_atacDigitoninBulk.R
@@ -6,30 +6,31 @@ setwd('~/Data/ATAC_seq_digitoninBulk/')
 source('~/Code/Rscripts/Templates/multiplot.R')
 
 dat = read.csv('~/Code/Rscripts/Postdoc/ATAC_seq/170404_bulkDigitonin/170406_atacDigitoninBulk.csv', header = T, row.names=1)
+dat$Percent_mtDNA = dat$Percent_mtDNA * 100
 
-p1 <- ggplot(dat, aes(factor(chr), Percent_mtDNA, fill = factor(Lysis_agent), label = factor(Sample))) +
-    geom_dotplot(binaxis = "y", stackdir = "center", position = "dodge") +
-    scale_fill_manual(values=c("blue", "red"), name="Lysis buffer",
-                      breaks=c("IGEPAL", "Digitonin"), labels=c("IGEPAL", "Digitonin")) +
-    ggtitle("Percent mtDNA ATAC-seq reads") + geom_text(check_overlap = TRUE, size = 3) +
-    xlab("groups") + ylab("% mtDNA reads") +
-    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(text = element_text(size=16))
+p1 <- ggplot(dat[!dat$Cell_line %in% "HCC38",], aes(factor(Lysis_agent), Percent_mtDNA, fill = factor(Cell_number))) +
+    geom_dotplot(binaxis = "y", stackdir = "center") +#, position = "dodge") +
+    scale_fill_manual(values=c("blue", "red"), name="Cell number",
+                      breaks=c("5000", "50000"), labels=c("5k", "50k")) +
+    ggtitle("") + xlab("Lysis buffer") + ylab("% mtDNA reads") +
+    theme_bw(base_size=28) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(text = element_text(size=28))
 p1
 
-p2 <- ggplot(dat, aes(factor(chr), ESTIMATED_LIBRARY_SIZE, fill = factor(Lysis_agent), label = factor(Sample))) +
-    geom_dotplot(binaxis = "y", stackdir = "center", position = "dodge") +
-    scale_fill_manual(values=c("blue", "red"), name="Lysis buffer",
-                      breaks=c("IGEPAL", "Digitonin"), labels=c("IGEPAL", "Digitonin")) +
-    ggtitle("Percent mtDNA ATAC-seq reads") + geom_text(check_overlap = TRUE, size = 3) +
-    xlab("groups") + ylab("% mtDNA reads") +
-    theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(text = element_text(size=16))
+p2 <- ggplot(dat[!dat$Cell_line %in% "HCC38",], aes(factor(Lysis_agent), ESTIMATED_LIBRARY_SIZE, fill = factor(Cell_number))) +
+    geom_dotplot(binaxis = "y", stackdir = "center") +#, position = "dodge") +
+    scale_fill_manual(values=c("blue", "red"), name="Cell number",
+                      breaks=c("5000", "50000"), labels=c("5k", "50k")) +
+    ggtitle("") + xlab("Lysis buffer") + ylab("Estimated library size") +
+    theme_bw(base_size=28) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(text = element_text(size=28))
 p2
 
 p3 <- ggplot(dat, aes(factor(chr), PERCENT_DUPLICATION, fill = factor(Lysis_agent), label = factor(Sample))) +
     geom_dotplot(binaxis = "y", stackdir = "center", position = "dodge") +
     scale_fill_manual(values=c("blue", "red"), name="Lysis buffer",
                       breaks=c("IGEPAL", "Digitonin"), labels=c("IGEPAL", "Digitonin")) +
-    ggtitle("Percent mtDNA ATAC-seq reads") + geom_text(check_overlap = TRUE, size = 3) +
-    xlab("groups") + ylab("% mtDNA reads") +
+    ggtitle("") + geom_text(check_overlap = TRUE, size = 3) +
+    xlab("groups") + ylab("% PCR duplicates") +
     theme_bw(base_size=16) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(text = element_text(size=16))
 p3
+
+#multiplot(p1,p2,p3, cols=1)
--- Templates/ggplot2.R
+++ Templates/ggplot2.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+source('~/Code/Rscripts/Templates/multiplot.R')
 
 #### Barchart ####
 plt = ggplot(data=bioRep[bioRep$PDGC %in% "MU020",], 
